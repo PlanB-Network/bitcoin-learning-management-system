7eab3189581d0308e33a9c16dc5fc86ae499c80a
guillaumehussong <planblockchain@gmail.com>
2023-05-31 08:58:28 +0200

    Finishing Builders section.

--- packages/api-server/src/services/content/builders.ts
+++ packages/api-server/src/services/content/builders.ts
@@ -1,12 +1,12 @@
-import { computeAssetRawUrl } from '@sovereign-academy/content';
-import { firstRow, getBuilderQuery } from '@sovereign-academy/database';
-import { JoinedBuilder } from '@sovereign-academy/types';
+import {computeAssetRawUrl} from '@sovereign-academy/content';
+import {firstRow, getBuilderQuery} from '@sovereign-academy/database';
+import {JoinedBuilder} from '@sovereign-academy/types';
 
-import { Dependencies } from '../../dependencies';
+import {Dependencies} from '../../dependencies';
 
 export const createGetBuilders =
     (dependencies: Dependencies) => async (language?: string) => {
-        const { postgres } = dependencies;
+        const {postgres} = dependencies;
 
         const result = await postgres<JoinedBuilder[]>`
       SELECT 
@@ -36,7 +36,7 @@ export const createGetBuilders =
 
 export const createGetBuilder =
     (dependencies: Dependencies) => async (id: number, language?: string) => {
-        const { postgres } = dependencies;
+        const {postgres} = dependencies;
 
         const builder = await postgres
             .exec(getBuilderQuery(id, language))
