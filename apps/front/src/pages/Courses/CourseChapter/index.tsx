f48884f75844583450b3a60f2320537e4fe6a63d
Lounès Ksouri <dev@louneskmt.com>
2023-06-03 23:11:05 +0200

    feat(front): responsive course chapter

--- apps/front/src/pages/Courses/CourseChapter/index.tsx
+++ apps/front/src/pages/Courses/CourseChapter/index.tsx
@@ -1,5 +1,9 @@
+import {
+  BreakPointHooks,
+  breakpointsTailwind,
+} from '@react-hooks-library/core';
 import { useTranslation } from 'react-i18next';
-import { BiSkipNext } from 'react-icons/bi';
+import { BiSkipNext, BiSkipPrevious } from 'react-icons/bi';
 import { BsCheckLg } from 'react-icons/bs';
 import ReactMarkdown, { uriTransformer } from 'react-markdown';
 import ReactPlayer from 'react-player/youtube';
@@ -14,10 +18,13 @@ import { Routes } from '../../../types';
 import { compose, useRequiredParams } from '../../../utils';
 import { CourseLayout } from '../CourseLayout';
 
+const { useGreater } = BreakPointHooks(breakpointsTailwind);
+
 export const CourseChapter = () => {
   const navigate = useNavigate();
   const { t, i18n } = useTranslation();
   const { courseId, language, chapterIndex } = useRequiredParams();
+  const isScreenMd = useGreater('sm');
 
   const { data: chapter, isFetched } = trpc.content.getCourseChapter.useQuery({
     courseId,
@@ -31,80 +38,129 @@ export const CourseChapter = () => {
     <CourseLayout>
       <div>
         {chapter && (
-          <div className="flex h-full w-full flex-col items-center justify-center px-2 py-10">
-            <div className="w-full max-w-5xl">
-              <h1 className="mb-5 w-full text-left text-5xl font-semibold text-orange-800">
+          <div className="flex h-full w-full flex-col items-center justify-center py-5 md:px-2 md:py-10">
+            <div className="w-full max-w-5xl px-5 md:px-0">
+              <h1 className="mb-5 w-full text-left text-3xl font-semibold text-orange-800 md:text-5xl">
                 {`${chapter.course?.id.toUpperCase()} - ${
                   chapter.course?.name
                 }`}
               </h1>
-              <div className="font-body flex flex-row justify-between text-lg font-thin tracking-wide">
-                <div>
-                  {t('courses.chapter.count', {
-                    count: chapter.chapter,
-                    total: chapter.course?.chapters?.length,
-                  })}
+              {isScreenMd ? (
+                <div className="font-body flex flex-row justify-between text-lg font-thin tracking-wide">
+                  <div>
+                    {t('courses.chapter.count', {
+                      count: chapter.chapter,
+                      total: chapter.course?.chapters?.length,
+                    })}
+                  </div>
+                  <div>{chapter.course?.teacher}</div>
                 </div>
-                <div>{chapter.course?.teacher}</div>
-              </div>
-              <div className="mb-16 mt-5 flex h-4 flex-row justify-between space-x-3 rounded-full">
-                {chapter.course?.chapters?.map((current, index) => {
-                  const firstChapter = current.chapter === 1;
-                  const lastChapter =
-                    current.chapter === chapter.course?.chapters?.length;
+              ) : (
+                <div className="flex flex-row items-center justify-between text-lg ">
+                  <Link
+                    className="h-6"
+                    to={
+                      chapter.chapter === 1
+                        ? generatePath(Routes.Course, {
+                            courseId,
+                          })
+                        : generatePath(Routes.CourseChapter, {
+                            courseId,
+                            chapterIndex: (chapter.chapter - 1).toString(),
+                          })
+                    }
+                  >
+                    <div className="bg-primary-700 flex h-6 flex-row items-center rounded-full px-3 py-2 text-white">
+                      <BiSkipPrevious className="h-6 w-6" />
+                    </div>
+                  </Link>
 
-                  if (current.chapter !== chapter.chapter)
+                  <div className="text-primary-800 font-normal">
+                    {t('courses.chapter.count', {
+                      count: chapter.chapter,
+                      total: chapter.course?.chapters?.length,
+                    })}
+                  </div>
+                  <Link
+                    className="h-6"
+                    to={
+                      chapter.chapter === chapter.course?.chapters?.length
+                        ? generatePath(Routes.Course, {
+                            courseId,
+                          })
+                        : generatePath(Routes.CourseChapter, {
+                            courseId,
+                            chapterIndex: (chapter.chapter + 1).toString(),
+                          })
+                    }
+                  >
+                    <div className="bg-primary-700 flex h-6 flex-row items-center rounded-full px-3 py-2 text-white">
+                      <BiSkipNext className="h-6 w-6" />
+                    </div>
+                  </Link>
+                </div>
+              )}
+
+              {isScreenMd && (
+                <div className="mt-5 flex h-4 flex-row justify-between space-x-3 rounded-full">
+                  {chapter.course?.chapters?.map((current, index) => {
+                    const firstChapter = current.chapter === 1;
+                    const lastChapter =
+                      current.chapter === chapter.course?.chapters?.length;
+
+                    if (current.chapter !== chapter.chapter)
+                      return (
+                        <Link
+                          className="h-4 grow"
+                          to={generatePath(Routes.CourseChapter, {
+                            courseId,
+                            chapterIndex: current.chapter.toString(),
+                          })}
+                        >
+                          <div
+                            key={index}
+                            className={compose(
+                              'h-4 grow',
+                              current.chapter < chapter.chapter
+                                ? 'bg-orange-600'
+                                : 'bg-gray-300',
+                              firstChapter ? 'rounded-l-full' : '',
+                              lastChapter ? 'rounded-r-full' : ''
+                            )}
+                          />
+                        </Link>
+                      );
+
                     return (
-                      <Link
-                        className="h-4 grow"
-                        to={generatePath(Routes.CourseChapter, {
-                          courseId,
-                          chapterIndex: current.chapter.toString(),
-                        })}
-                      >
+                      <div className="relative flex grow overflow-visible">
                         <div
-                          key={index}
                           className={compose(
-                            'h-4 grow',
-                            current.chapter < chapter.chapter
-                              ? 'bg-orange-600'
-                              : 'bg-gray-300',
-                            firstChapter ? 'rounded-l-full' : '',
+                            'h-4 w-2/3 bg-orange-600',
+                            firstChapter ? 'rounded-l-full' : ''
+                          )}
+                        />
+                        <div
+                          className={compose(
+                            'h-4 w-1/3 bg-gray-300',
                             lastChapter ? 'rounded-r-full' : ''
                           )}
                         />
-                      </Link>
+                        <ProgressRabbit className="absolute inset-0 bottom-4 m-auto h-12 w-full" />
+                      </div>
                     );
-
-                  return (
-                    <div className="relative flex grow overflow-visible">
-                      <div
-                        className={compose(
-                          'h-4 w-2/3 bg-orange-600',
-                          firstChapter ? 'rounded-l-full' : ''
-                        )}
-                      />
-                      <div
-                        className={compose(
-                          'h-4 w-1/3 bg-gray-300',
-                          lastChapter ? 'rounded-r-full' : ''
-                        )}
-                      />
-                      <ProgressRabbit className="absolute inset-0 bottom-4 m-auto h-12 w-full" />
-                    </div>
-                  );
-                })}
-              </div>
+                  })}
+                </div>
+              )}
             </div>
-            <div className="max-w-3xl">
-              <h2 className="text-primary-800 mb-5 text-3xl font-normal uppercase italic">
+            <div className="mt-8 w-full px-5 md:mt-16 md:max-w-3xl md:px-0">
+              <h2 className="text-primary-800 mb-5 text-2xl font-normal uppercase italic md:text-3xl">
                 {chapter?.title}
               </h2>
               <ReactMarkdown
                 children={chapter?.raw_content}
                 components={{
                   h2: ({ children }) => (
-                    <h2 className="text-primary-900 mb-5 mt-10 text-2xl font-semibold">
+                    <h2 className="text-primary-900 mb-5 mt-6 text-xl font-semibold md:mt-10 md:text-2xl">
                       {children}
                     </h2>
                   ),
@@ -122,7 +178,7 @@ export const CourseChapter = () => {
                     src?.includes('youtube.com') ||
                     src?.includes('youtu.be') ? (
                       <ReactPlayer
-                        className="mx-auto my-4 flex justify-center rounded-lg"
+                        className="mx-auto my-12 flex max-w-full	justify-center rounded-lg"
                         controls={true}
                         url={src}
                       />
