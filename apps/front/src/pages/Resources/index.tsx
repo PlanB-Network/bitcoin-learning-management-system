c0c45b87bd95efa58cbf28fb0714dca23232009b
Lounès Ksouri <dev@louneskmt.com>
2023-06-04 10:42:51 +0200

    fix(front): responsive resources and tutorials

--- apps/front/src/pages/Resources/index.tsx
+++ apps/front/src/pages/Resources/index.tsx
@@ -71,42 +71,48 @@ export const Resources = () => {
   const { t } = useTranslation();
 
   return (
-    <MainLayout footerVariant="dark">
-      <div className="bg-primary-700 flex flex-col justify-center">
-        <div className="bg-primary-900 flex flex-wrap justify-evenly px-6 pb-12 pt-8 text-white sm:pb-40">
-          <div>
-            <h1 className="-ml-6 text-[62px] font-thin xl:text-[128px]">
-              {t('resources.pageTitle')}
-            </h1>
-            <div className="max-w-sm space-y-6 text-justify text-sm">
-              <p>{t('resources.headerText')}</p>
-              <p>{t('resources.headerSignature')}</p>
-            </div>
-          </div>
-          <img className="mt-6 max-h-96" src={rabbitInLibrary} />
-        </div>
-
-        <div className="bg-primary-700 flex w-[950px] max-w-[90vw] flex-wrap justify-evenly self-center py-8">
-          {resourceKinds.map((resourceKind, i) => (
-            <Link key={i} to={resourceKind.route}>
-              <div
-                className="hover:bg-primary-600 relative my-4 box-content flex h-16 w-44 cursor-pointer flex-row rounded-lg p-2 duration-300 sm:h-24 sm:w-60"
-                key={resourceKind.kind}
-              >
-                <div className="bg-secondary-400 absolute z-0 flex h-20 w-20 rounded-full sm:h-24 sm:w-24">
-                  <img
-                    className="m-auto h-12 sm:h-16"
-                    src={resourceKind.image}
-                  />
-                </div>
-                <div className="relative ml-10 flex flex-row items-center sm:ml-14">
-                  <h3 className="relative z-10 ml-12 text-sm text-white sm:text-2xl">
-                    {resourceKind.title}
-                  </h3>
-                </div>
+    <MainLayout footerVariant="light">
+      <div className="flex flex-col justify-center bg-gray-100">
+        <div className="bg-primary-900 relative mb-10 flex flex-col items-center px-5 pb-10 pt-8 text-white md:mb-80 md:pb-96 lg:px-16 lg:pb-80">
+          <div className="flex w-full grid-cols-2 flex-col items-center justify-evenly md:grid md:pl-8 lg:space-x-5 lg:pl-12">
+            <div className="px-5 lg:px-0">
+              <h1 className="z-10 -ml-6 mb-5 text-[62px] font-thin md:text-7xl lg:text-8xl xl:text-[112px]">
+                {t('resources.pageTitle')}
+              </h1>
+              <div className="space-y-6 text-justify text-base md:max-w-xs lg:max-w-sm xl:max-w-md">
+                <p>{t('resources.headerText')}</p>
+                <p>{t('resources.headerSignature')}</p>
               </div>
-            </Link>
-          ))}
+            </div>
+            <img
+              className="z-0 mb-10 mt-6 max-h-72 md:max-h-60 lg:max-h-80 xl:max-h-96"
+              src={rabbitInLibrary}
+              alt=""
+            />
+          </div>
+          <div className="bg-primary-700 inset-x-0 bottom-0 left-1/2 z-10 flex w-full max-w-min flex-row flex-wrap justify-evenly rounded-3xl px-12 py-8 shadow md:absolute md:max-w-3xl md:-translate-x-1/2 md:translate-y-1/2 lg:max-w-5xl">
+            {resourceKinds.map((resourceKind, i) => (
+              <Link key={i} to={resourceKind.route}>
+                <div
+                  className="hover:bg-primary-600 relative my-4 box-content flex h-16 w-44 cursor-pointer flex-row rounded-lg p-2 duration-300 sm:h-24 sm:w-60"
+                  key={resourceKind.kind}
+                >
+                  <div className="bg-secondary-400 absolute z-0 flex h-20 w-20 rounded-full sm:h-24 sm:w-24">
+                    <img
+                      className="m-auto h-12 sm:h-16"
+                      src={resourceKind.image}
+                      alt=""
+                    />
+                  </div>
+                  <div className="relative ml-10 flex flex-row items-center sm:ml-14">
+                    <h3 className=" relative ml-12 text-sm text-white sm:text-2xl">
+                      {resourceKind.title}
+                    </h3>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </MainLayout>
--- apps/front/src/pages/Tutorials/index.tsx
+++ apps/front/src/pages/Tutorials/index.tsx
@@ -64,9 +64,9 @@ export const Tutorials = () => {
     <MainLayout footerVariant="light">
       <div className=" flex flex-col justify-center bg-gray-100">
         <div className="bg-primary-900 relative mb-10 flex flex-col items-center px-5 pb-10 pt-8 text-white md:mb-60 md:pb-80 lg:px-16 lg:pb-60">
-          <div className="flex w-full flex-wrap items-center justify-evenly lg:space-x-5">
+          <div className="flex w-full grid-cols-2 flex-col flex-wrap items-center justify-evenly md:grid md:pl-8 lg:space-x-5 lg:pl-12">
             <div className="px-5 lg:px-0">
-              <h1 className="-ml-6 mb-5 text-[62px] font-thin md:text-7xl lg:text-8xl xl:text-[128px]">
+              <h1 className="z-10 -ml-6 mb-5 text-[62px] font-thin md:text-7xl lg:text-8xl xl:text-[112px]">
                 {t('tutorials.pageTitle')}
               </h1>
               <div className="space-y-6 text-justify text-base md:max-w-xs lg:max-w-sm xl:max-w-md">
@@ -75,7 +75,7 @@ export const Tutorials = () => {
               </div>
             </div>
             <img
-              className="mb-10 mt-6 max-h-72 md:max-h-60 lg:max-h-80 xl:max-h-96"
+              className="z-0 mb-10 mt-6 max-h-72 md:max-h-60 lg:max-h-80 xl:max-h-96"
               src={tutoRabbitPng}
               alt=""
             />
