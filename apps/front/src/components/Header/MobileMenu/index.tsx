4323d39fcce54d6b69d64957dbed35e6616df7e2
Lounès Ksouri <dev@louneskmt.com>
2023-06-04 10:17:40 +0200

    fix(front): responsive mobile menu and tutorials

--- apps/front/src/components/Header/MobileMenu/index.tsx
+++ apps/front/src/components/Header/MobileMenu/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
@@ -24,6 +25,13 @@ export const MobileMenu = ({
   const { isOpen: isMobileMenuOpen, toggle: toggleMobileMenu } =
     useDisclosure();
 
+  useEffect(() => {
+    if (isMobileMenuOpen) document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'scroll';
+    };
+  });
+
   return (
     <>
       <div className="flex w-full flex-row justify-center">
@@ -37,9 +45,8 @@ export const MobileMenu = ({
       </div>
       <FaBars
         className={compose(
-          'absolute z-40 cursor-pointer left-[4vw]',
-          isMobileMenuOpen ? 'rotate-90' : 'rotate-0',
-          isMobileMenuOpen ? 'text-primary-800' : 'text-white'
+          'absolute z-40 cursor-pointer left-[6vw] text-white',
+          isMobileMenuOpen ? 'rotate-90' : 'rotate-0'
         )}
         style={{
           transition: 'transform 0.4s, color 0.2s',
@@ -50,11 +57,11 @@ export const MobileMenu = ({
       />
       <nav
         className={compose(
-          'flex fixed top-0 left-0 flex-col items-center px-2 pt-28 pb-5 w-screen h-screen bg-white duration-300',
+          'flex fixed top-0 left-0 flex-col items-center px-2 pt-28 pb-5 w-screen h-full bg-primary-900 duration-300',
           isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'
         )}
       >
-        <ul className="my-0 flex-1 list-none overflow-auto pl-0">
+        <ul className="my-0 flex-1 list-none space-y-6 overflow-auto pl-10">
           {sections.map((section) => (
             <MobileMenuSection section={section} key={section.id} />
           ))}
--- apps/front/src/pages/Tutorials/index.tsx
+++ apps/front/src/pages/Tutorials/index.tsx
@@ -7,6 +7,7 @@ import merchantSvg from '../../assets/tutorials/merchant.svg';
 import miningSvg from '../../assets/tutorials/mining.svg';
 import nodeSvg from '../../assets/tutorials/node.svg';
 import walletSvg from '../../assets/tutorials/wallet.svg';
+import { Card } from '../../atoms/Card';
 import { MainLayout } from '../../components';
 import { Routes } from '../../types';
 
@@ -60,40 +61,51 @@ export const Tutorials = () => {
   const { t } = useTranslation();
 
   return (
-    <MainLayout footerVariant="dark">
-      <div className="bg-primary-700 flex flex-col justify-center">
-        <div className="bg-primary-900 flex flex-wrap justify-evenly px-6 pb-12 pt-8 text-white sm:pb-40">
-          <div>
-            <h1 className="-ml-6 text-[62px] font-thin xl:text-[128px]">
-              {t('tutorials.pageTitle')}
-            </h1>
-            <div className="max-w-sm space-y-6 text-justify text-sm">
-              <p>{t('tutorials.headerText')}</p>
-              <p>{t('tutorials.headerSignature')}</p>
+    <MainLayout footerVariant="light">
+      <div className=" flex flex-col justify-center bg-gray-100">
+        <div className="bg-primary-900 relative mb-10 flex flex-col items-center px-5 pb-10 pt-8 text-white md:mb-60 md:pb-80 lg:px-16 lg:pb-60">
+          <div className="flex w-full flex-wrap items-center justify-evenly lg:space-x-5">
+            <div className="px-5 lg:px-0">
+              <h1 className="-ml-6 mb-5 text-[62px] font-thin md:text-7xl lg:text-8xl xl:text-[128px]">
+                {t('tutorials.pageTitle')}
+              </h1>
+              <div className="space-y-6 text-justify text-base md:max-w-xs lg:max-w-sm xl:max-w-md">
+                <p>{t('tutorials.headerText')}</p>
+                <p>{t('tutorials.headerSignature')}</p>
+              </div>
             </div>
+            <img
+              className="mb-10 mt-6 max-h-72 md:max-h-60 lg:max-h-80 xl:max-h-96"
+              src={tutoRabbitPng}
+              alt=""
+            />
           </div>
-          <img className="mt-6 max-h-96" src={tutoRabbitPng} />
-        </div>
-
-        <div className="bg-primary-700 ml-[50%] box-content flex w-[950px] max-w-[90vw] -translate-x-1/2 flex-row flex-wrap justify-evenly rounded-[50px] px-12 py-8">
-          {tutorialKinds.map((tutorialKind) => (
-            <Link to={tutorialKind.route}>
-              <div
-                className="hover:bg-primary-600 relative my-4 box-content flex h-24 w-64 cursor-pointer flex-row rounded-lg p-2 duration-300"
-                key={tutorialKind.kind}
-              >
-                <div className="bg-secondary-400 absolute z-0 flex h-24 w-24 rounded-full">
-                  <img className="m-auto h-16" src={tutorialKind.image} />
-                </div>
-                <div className="relative -ml-8 mt-4 flex flex-row items-center">
-                  <div className="relative z-10 ml-32 pl-2 text-white">
-                    <h3 className="text-2xl">{tutorialKind.title}</h3>
-                    <p className="text-xs italic">{tutorialKind.description}</p>
+          <div className="bg-primary-700 inset-x-0 bottom-0 left-1/2 z-10 flex w-full max-w-min flex-row flex-wrap justify-evenly rounded-3xl px-12 py-8 shadow md:absolute md:max-w-3xl md:-translate-x-1/2 md:translate-y-1/2 lg:max-w-5xl">
+            {tutorialKinds.map((tutorialKind) => (
+              <Link to={tutorialKind.route}>
+                <div
+                  className="hover:bg-primary-600 relative my-4 box-content flex h-24 w-64 cursor-pointer flex-row rounded-lg p-2 duration-300"
+                  key={tutorialKind.kind}
+                >
+                  <div className="bg-secondary-400 absolute z-0 flex h-24 w-24 rounded-full">
+                    <img
+                      className="m-auto h-16"
+                      src={tutorialKind.image}
+                      alt=""
+                    />
+                  </div>
+                  <div className="relative z-10 -ml-8 mt-4 flex flex-row items-center">
+                    <div className="relative ml-32 pl-2 text-white">
+                      <h3 className="text-2xl">{tutorialKind.title}</h3>
+                      <p className="text-xs italic">
+                        {tutorialKind.description}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </MainLayout>
