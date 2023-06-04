8252811a119de2e7d5783926e3029c0c27f6b64b
ac1dbunny21 <github@acidbunny21.xyz>
2023-06-04 12:38:27 +0200

    feat(front): i18n home page

--- apps/front/src/pages/Home/index.tsx
+++ apps/front/src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 import community from '../../assets/home/community.svg';
 import bitcoinCurrency from '../../assets/home/currency_bitcoin.svg';
 import favorite from '../../assets/home/favorite.svg';
@@ -12,9 +14,12 @@ import rabbitWithBackpackAndBtcSign from '../../assets/home/rabbit-with-backpack
 import visibilityOff from '../../assets/home/visibility_off.svg';
 import { MainLayout } from '../../components';
 
+import { Emphasize } from './Emphasize';
 import { SectionTitle } from './SectionTitle';
 
 export const Home = () => {
+  const { t } = useTranslation();
+
   return (
     <MainLayout footerVariant="dark">
       {/* Hero section */}
@@ -24,7 +29,7 @@ export const Home = () => {
             The Sovereign University
           </h1>
           <p className=" mt-10 text-base font-thin italic tracking-wide text-white sm:text-xl lg:text-2xl">
-            Let’s embark into your Bitcoin educational journey
+            {t('home.hero.tagLine')}
           </p>
         </div>
         <div className="ml-36 mt-12 sm:mt-36">
@@ -38,8 +43,8 @@ export const Home = () => {
       {/* 1rst Section */}
       <div className="bg-primary-800  px-6 pb-12 pt-6 sm:px-12 md:px-20">
         <SectionTitle
-          title="E-learning platform about Bitcoin"
-          tagLine="To learn the basics as well as the advanced"
+          title={t('home.first.title')}
+          tagLine={t('home.first.tagLine')}
           num={1}
         />
 
@@ -49,21 +54,25 @@ export const Home = () => {
           </div>
           <ul className="ml-0 mt-6 list-disc space-y-4 text-lg text-white sm:text-xl md:ml-16 md:mt-0 lg:ml-24 lg:text-2xl">
             <li>
-              <span className="text-secondary-400 uppercase">Extensive</span>{' '}
-              courses
+              <Emphasize words={[t('home.first.extensiveEmphasized')]}>
+                {t('home.first.extensive')}
+              </Emphasize>
             </li>
             <li>
-              All <span className="text-secondary-400 uppercase">free</span>
+              <Emphasize words={[t('home.first.freeEmphasized')]}>
+                {t('home.first.free')}
+              </Emphasize>
             </li>
             <li>
-              For <span className="text-secondary-400 uppercase">everyone</span>
+              <Emphasize words={[t('home.first.forEveryoneEmphasized')]}>
+                {t('home.first.forEveryone')}
+              </Emphasize>
             </li>
-            <li>In many languages</li>
+            <li>{t('home.first.languages')}</li>
             <li>
-              From{' '}
-              <span className="text-secondary-400 uppercase">
-                beginner to expert
-              </span>
+              <Emphasize words={[t('home.first.levelsEmphasized')]}>
+                {t('home.first.levels')}
+              </Emphasize>
             </li>
           </ul>
         </div>
@@ -72,8 +81,8 @@ export const Home = () => {
       {/* 2nd section */}
       <div className="bg-primary-900 px-6 pb-24 pt-20 sm:px-12 md:px-20">
         <SectionTitle
-          title="All useful tutorials"
-          tagLine="To navigate your way among Bitcoin technological solutions"
+          title={t('home.second.title')}
+          tagLine={t('home.second.tagLine')}
           num={2}
           position="right"
           variant="secondary"
@@ -81,12 +90,19 @@ export const Home = () => {
 
         <div className="my-12 flex flex-col-reverse items-center justify-center md:flex-row">
           <p className="text-lg leading-[50px] text-white sm:text-2xl">
-            Exchange <span className="text-secondary-400">|</span> Merchant
-            <br />
-            Mining <span className="text-secondary-400">|</span> Node{' '}
-            <span className="text-secondary-400">|</span> Nostfr
-            <br />
-            Privacy tools <span className="text-secondary-400">|</span> Wallet
+            {[
+              t('home.second.exchange'),
+              t('home.second.merchant'),
+              t('home.second.mining'),
+              t('home.second.node'),
+              t('home.second.privacy'),
+              t('home.second.wallet'),
+            ].map((w, index) => (
+              <>
+                <span>{w}</span> <Emphasize>|</Emphasize>{' '}
+                {index % 2 !== 0 && <br />}
+              </>
+            ))}
           </p>
 
           <div className="my-12 max-w-sm md:my-0 md:ml-24 lg:max-w-xl">
@@ -98,8 +114,8 @@ export const Home = () => {
       {/* 3rd section */}
       <div className="bg-primary-800 px-6 pb-16 pt-6 sm:px-12 md:px-20">
         <SectionTitle
-          title="All kinds of resources about Bitcoin"
-          tagLine="To access all published knowledge in one open-source platform"
+          title={t('home.third.title')}
+          tagLine={t('home.third.tagLine')}
           num={3}
         />
 
@@ -111,7 +127,7 @@ export const Home = () => {
                 src={rabbitLibrary}
                 alt="a rabbit reading a book"
               />
-              <p className="my-2 text-xl sm:my-8">books</p>
+              <p className="my-2 text-xl sm:my-8">{t('home.third.books')}</p>
             </div>
             <div className="mx-auto mt-4 flex flex-col items-center text-white md:mx-0">
               <img
@@ -119,7 +135,7 @@ export const Home = () => {
                 src={rabbitPodcast}
                 alt="a rabbit listenning to a podcast"
               />
-              <p className="my-2 text-xl sm:my-8">podcasts</p>
+              <p className="my-2 text-xl sm:my-8">{t('home.third.podcasts')}</p>
             </div>
             <div className="mx-auto mt-4 flex flex-col items-center text-white md:mx-0">
               <img
@@ -127,11 +143,11 @@ export const Home = () => {
                 src={rabbitVideos}
                 alt="a rabbit filming a video"
               />
-              <p className="my-2 text-xl sm:my-8">videos</p>
+              <p className="my-2 text-xl sm:my-8">{t('home.third.videos')}</p>
             </div>
           </div>
           <p className="text-secondary-400 mx-auto mt-12 w-max italic md:mt-24">
-            & more
+            {t('home.third.more')}
           </p>
         </div>
       </div>
@@ -139,8 +155,8 @@ export const Home = () => {
       {/* 4rth section */}
       <div className="bg-primary-900 px-6 pb-24 pt-16 sm:px-12 md:px-20">
         <SectionTitle
-          title="A link to the builders of the industry"
-          tagLine="To connect learners with their dream Bitcoin job"
+          title={t('home.fourth.title')}
+          tagLine={t('home.fourth.tagLine')}
           num={4}
           position="right"
           variant="secondary"
@@ -152,42 +168,31 @@ export const Home = () => {
           </div>
           <div>
             <p className="max-w-xs text-lg text-white sm:text-xl sm:leading-10 lg:text-2xl lg:leading-[50px]">
-              Get an overview and connect with the different actors of the
-              industry
+              {t('home.fourth.text')}
             </p>
           </div>
         </div>
       </div>
 
       {/* About us */}
-      <div className="bg-primary-700 px-6 pb-32 pt-16 text-white sm:px-12 md:px-20 lg:pt-6">
+      <div className="bg-primary-700 px-6 pb-32 pt-16 text-white sm:px-12 md:px-20">
         <h2 className="mb-16 text-center text-4xl italic ">
-          A little bit about us
+          {t('home.about.title')}
         </h2>
 
-        <div className="mb-16 max-w-3xl space-y-6 px-6 sm:px-0">
-          <p>
-            The Sovereign University aims at providing a Bitcoin high-quality,
-            extensive, free and accessible education for all. We believe that
-            education and comprehension is the key to mass adoption.
-          </p>
-          <p>As Bitcoin grows stronger everyday, so does its community.</p>
-          <p>
-            Community is our biggest strength, as our educational content is
-            based upon volunteering teachers, passionate about transmitting
-            their knowledge.
-          </p>
-          <p>
-            The Sovereign University is a project powered by DécouvreBitcoin.
-          </p>
+        <div className="mx-auto mb-16 max-w-3xl space-y-6 px-6 text-justify sm:px-0">
+          <p>{t('home.about.p1')}</p>
+          <p>{t('home.about.p2')}</p>
+          <p>{t('home.about.p3')}</p>
+          <p>{t('home.about.p4')}</p>
         </div>
 
         <div className=" mx-auto mt-28 flex w-max grid-cols-3 grid-rows-3 flex-col space-y-12 px-12 md:grid md:space-y-0 lg:mt-36">
           <div className="col-start-1 flex max-w-[180px] flex-col items-center space-y-4 lg:max-w-[250px]">
             <img className="h-8" src={github} alt="the icon of github" />
-            <h4>OPEN-SOURCE</h4>
+            <h4>{t('home.about.values.openSource.title')}</h4>
             <p className="text-center">
-              We aim to promote the free flow of knowledge.
+              {t('home.about.values.openSource.text')}
             </p>
           </div>
           <div className="col-start-3 flex max-w-[180px] flex-col items-center space-y-4 lg:max-w-[250px]">
@@ -196,14 +201,13 @@ export const Home = () => {
               src={bitcoinCurrency}
               alt="the sign of bitcoin currency"
             />
-            <h4>BITCOIN-ONLY</h4>
+            <h4>{t('home.about.values.bitcoinOnly.title')}</h4>
             <p className="text-center">
-              We focus on the Bitcoin revolution as a whole without falling into
-              trading or shitcoinery.
+              {t('home.about.values.bitcoinOnly.text')}
             </p>
           </div>
           <div className="relative col-start-2 row-start-2 flex flex-col items-center justify-center">
-            <div className="bg-primary-900 absolute left-1/2 top-1/2 h-36 w-36 -translate-x-1/2 -translate-y-1/2 rounded-full" />
+            <div className="bg-primary-900 absolute left-1/2 top-1/2 h-36 w-36 -translate-x-1/2 -translate-y-1/2 rounded-full lg:h-48 lg:w-48" />
             <img
               className="relative top-1 z-10 my-12 h-16 md:my-0 md:h-24"
               src={favorite}
@@ -216,18 +220,16 @@ export const Home = () => {
               src={visibilityOff}
               alt="the eyes icon without off sign"
             />
-            <h4>PRIVACY FOCUSED</h4>
+            <h4>{t('home.about.values.privacyFocused.title')}</h4>
             <p className="text-center">
-              We consider privacy to be one of the most fundamental human rights
-              and most necessary for progress.
+              {t('home.about.values.privacyFocused.text')}
             </p>
           </div>
           <div className="col-start-3 row-start-3 flex max-w-[180px] flex-col items-center space-y-4 lg:max-w-[250px]">
             <img className="h-8" src={community} alt="3 people" />
-            <h4>COMMUNITY DRIVEN</h4>
+            <h4>{t('home.about.values.communityDriven.title')}</h4>
             <p className="text-center">
-              We are first & foremost a community of enthusiastics helping each
-              other in their Bitcoin journey.
+              {t('home.about.values.communityDriven.text')}
             </p>
           </div>
         </div>
