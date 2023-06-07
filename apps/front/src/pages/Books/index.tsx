a96472008841275ae5126fa3c8c9fa6d2a1ad519
Lounès Ksouri <dev@louneskmt.com>
2023-06-07 11:51:55 +0200

    fix: search for podcasts and books

--- apps/front/src/pages/Books/index.tsx
+++ apps/front/src/pages/Books/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { Link, generatePath } from 'react-router-dom';
 
 import { trpc } from '@sovereign-academy/api-client';
+import { JoinedBook } from '@sovereign-academy/types';
 
 import { ResourceLayout } from '../../components';
 import { Routes } from '../../types';
@@ -14,88 +15,61 @@ export const Books = () => {
   // Fetching data from the API
   const { data: books } = trpc.content.getBooks.useQuery();
 
-  // Adding a category to each builder
-  const sortedBooks = books
+  // Sort books alphabetically
+  const sortedBooks: JoinedBook[] = books
     ? books.sort((a, b) => a.title.localeCompare(b.title))
     : [];
-
-  const categorizedBooks = sortedBooks.reduce((acc, book) => {
-    if (!acc[book.title]) {
-      acc[book.title] = [];
-    }
-    acc[book.title].push(book);
-    return acc;
-  }, {} as Record<string, typeof sortedBooks>);
-
-  const categories = [...new Set(sortedBooks.map((book) => book.title))].sort(
-    (a, b) => categorizedBooks[b].length - categorizedBooks[a].length
-  );
 
   return (
     <ResourceLayout
       title={t('library.pageTitle')}
       tagLine={t('library.pageSubtitle')}
       filterBar={{
-        onChange: () => setSearchTerm,
+        onChange: setSearchTerm,
         label: t('resources.filterBarLabel'),
       }}
     >
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 md:gap-8 lg:grid-cols-5">
-        {categories.map((category) => {
-          const filteredBooks = categorizedBooks[category].filter((book) =>
+        {sortedBooks
+          .filter((book) =>
             book.title.toLowerCase().includes(searchTerm.toLowerCase())
-          );
-
-          // If no result, do not show an empty card.
-          if (filteredBooks.length === 0) {
-            return null;
-          }
-
-          return (
-            <div>
-              {filteredBooks.map((book, index) => (
-                <Link
-                  className="group z-10 m-auto mx-2 h-fit w-20 min-w-[100px] delay-100 hover:z-20 hover:delay-0"
-                  to={generatePath(Routes.Book, {
-                    bookId: book.id.toString(),
-                    language: book.language,
-                  })}
-                  key={book.id}
-                >
-                  <div className="group-hover:bg-secondary-400 z-10 mb-2 h-fit px-2 pt-2 transition duration-500 ease-in-out group-hover:scale-125">
-                    <img
-                      className="mx-auto"
-                      src={book.cover}
-                      alt={book.title}
-                    />
-                    <div className="group-hover:bg-secondary-400 absolute inset-x-0 rounded-b-lg px-4 py-2 text-left text-xs font-thin text-white transition-colors duration-500 ease-in-out">
-                      <ul className="opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100">
-                        <li className={'pb-1 text-lg font-bold'}>
-                          {book.title}
+          )
+          .map((book) => (
+            <div key={book.id}>
+              <Link
+                className="group z-10 m-auto mx-2 h-fit w-20 min-w-[100px] delay-100 hover:z-20 hover:delay-0"
+                to={generatePath(Routes.Book, {
+                  bookId: book.id.toString(),
+                  language: book.language,
+                })}
+                key={book.id}
+              >
+                <div className="group-hover:bg-secondary-400 z-10 mb-2 h-fit px-2 pt-2 transition duration-500 ease-in-out group-hover:scale-125">
+                  <img className="mx-auto" src={book.cover} alt={book.title} />
+                  <div className="group-hover:bg-secondary-400 absolute inset-x-0 rounded-b-lg px-4 py-2 text-left text-xs font-thin text-white transition-colors duration-500 ease-in-out">
+                    <ul className="opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100">
+                      <li className={'pb-1 text-lg font-bold'}>{book.title}</li>
+                      {book.author && (
+                        <li className={'pb-1 text-xs italic'}>
+                          {t('library.writtenBy', { author: book.author })}
                         </li>
-                        {book.author && (
-                          <li className={'pb-1 text-xs italic'}>
-                            {t('library.writtenBy', { author: book.author })}
-                          </li>
-                        )}
-                        {book.publication_year && (
-                          <li className={'pb-1 text-xs italic'}>
-                            {t('library.publishedIn', {
-                              date: book.publication_year,
-                            })}
-                          </li>
-                        )}
-                        {/* <li className={'truncate pb-1 text-xs'}>
+                      )}
+                      {book.publication_year && (
+                        <li className={'pb-1 text-xs italic'}>
+                          {t('library.publishedIn', {
+                            date: book.publication_year,
+                          })}
+                        </li>
+                      )}
+                      {/* <li className={'truncate pb-1 text-xs'}>
                           {book.description}
                         </li> */}
-                      </ul>
-                    </div>
+                    </ul>
                   </div>
-                </Link>
-              ))}
+                </div>
+              </Link>
             </div>
-          );
-        })}
+          ))}
       </div>
     </ResourceLayout>
   );
--- apps/front/src/pages/Podcasts/index.tsx
+++ apps/front/src/pages/Podcasts/index.tsx
@@ -3,96 +3,73 @@ import { useTranslation } from 'react-i18next';
 import { Link, generatePath } from 'react-router-dom';
 
 import { trpc } from '@sovereign-academy/api-client';
+import { JoinedPodcast } from '@sovereign-academy/types';
 
 import { ResourceLayout } from '../../components';
 import { Routes } from '../../types';
 
 export const Podcasts = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const [searchTerm, setSearchTerm] = useState('');
 
   // Fetching data from the API
   const { data: podcasts } = trpc.content.getPodcasts.useQuery({
-    language: 'en',
+    language: i18n.language ?? 'en',
   });
 
-  // Adding a category to each builder
-  const sortedPodcasts = podcasts
+  // Sort podcasts alphabetically
+  const sortedPodcasts: JoinedPodcast[] = podcasts
     ? podcasts.sort((a, b) => a.name.localeCompare(b.name))
     : [];
-
-  const categorizedPodcasts = sortedPodcasts.reduce((acc, podcast) => {
-    if (!acc[podcast.name]) {
-      acc[podcast.name] = [];
-    }
-    acc[podcast.name].push(podcast);
-    return acc;
-  }, {} as Record<string, typeof sortedPodcasts>);
-
-  const categories = [
-    ...new Set(sortedPodcasts.map((podcast) => podcast.name)),
-  ].sort(
-    (a, b) => categorizedPodcasts[b].length - categorizedPodcasts[a].length
-  );
 
   return (
     <ResourceLayout
       title={t('podcasts.pageTitle')}
       tagLine={t('podcasts.pageSubtitle')}
       filterBar={{
-        onChange: () => setSearchTerm,
+        onChange: setSearchTerm,
         label: t('resources.filterBarLabel'),
       }}
     >
       <div className="grid grid-cols-2 gap-4 px-10 sm:grid-cols-3 sm:px-0 md:grid-cols-4 md:gap-8 lg:grid-cols-5">
-        {categories.map((category) => {
-          const filteredPodcats = categorizedPodcasts[category].filter(
-            (podcast) =>
-              podcast.name.toLowerCase().includes(searchTerm.toLowerCase())
-          );
-
-          // If no result, do not show an empty card.
-          if (filteredPodcats.length === 0) {
-            return null;
-          }
-
-          return (
+        {sortedPodcasts
+          .filter((podcast) =>
+            podcast.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((podcast) => (
             <div>
-              {filteredPodcats.map((podcast) => (
-                <Link
-                  className="group z-10 m-auto mx-2 mb-5 h-fit w-20 min-w-[100px] delay-100 hover:z-20 hover:delay-0"
-                  to={generatePath(Routes.Podcast, {
-                    podcastId: podcast.id.toString(),
-                  })}
-                  key={podcast.id}
-                >
-                  <div className="group-hover:bg-secondary-400 z-10 mb-2 h-fit px-2 pt-2 transition duration-500 ease-in-out group-hover:scale-125">
-                    <img
-                      className="mx-auto"
-                      src={podcast.logo}
-                      alt={podcast.name}
-                    />
-                    <div className="group-hover:bg-secondary-400 absolute inset-x-0 rounded-b-lg px-4 py-2 text-left text-xs font-thin text-white transition-colors duration-500 ease-in-out">
-                      <ul className="opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100">
-                        <li className={'pb-1 text-lg font-bold'}>
-                          {podcast.name}
+              <Link
+                className="group z-10 m-auto mx-2 mb-5 h-fit w-20 min-w-[100px] delay-100 hover:z-20 hover:delay-0"
+                to={generatePath(Routes.Podcast, {
+                  podcastId: podcast.id.toString(),
+                })}
+                key={podcast.id}
+              >
+                <div className="group-hover:bg-secondary-400 z-10 mb-2 h-fit px-2 pt-2 transition duration-500 ease-in-out group-hover:scale-125">
+                  <img
+                    className="mx-auto"
+                    src={podcast.logo}
+                    alt={podcast.name}
+                  />
+                  <div className="group-hover:bg-secondary-400 absolute inset-x-0 rounded-b-lg px-4 py-2 text-left text-xs font-thin text-white transition-colors duration-500 ease-in-out">
+                    <ul className="opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100">
+                      <li className={'pb-1 text-lg font-bold'}>
+                        {podcast.name}
+                      </li>
+                      {podcast.host && (
+                        <li className={'pb-1 text-xs italic'}>
+                          {t('podcasts.hostedBy', { host: podcast.host })}
                         </li>
-                        {podcast.host && (
-                          <li className={'pb-1 text-xs italic'}>
-                            {t('podcasts.hostedBy', { host: podcast.host })}
-                          </li>
-                        )}
-                        {/* <li className={'truncate pb-1 text-xs'}>
+                      )}
+                      {/* <li className={'truncate pb-1 text-xs'}>
                           {podcast.description}
                         </li> */}
-                      </ul>
-                    </div>
+                    </ul>
                   </div>
-                </Link>
-              ))}
+                </div>
+              </Link>
             </div>
-          );
-        })}
+          ))}
       </div>
     </ResourceLayout>
   );
