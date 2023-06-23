55feea1a720f9945c39e669b89a81ad3b40db836
Lounès Ksouri <dev@louneskmt.com>
2023-06-23 22:09:31 +0200

    feat(front): better display some basic md elements (lists, code, tables)

--- apps/front/src/pages/Courses/CourseChapter/index.tsx
+++ apps/front/src/pages/Courses/CourseChapter/index.tsx
@@ -8,6 +8,11 @@ import { BsCheckLg } from 'react-icons/bs';
 import ReactMarkdown, { uriTransformer } from 'react-markdown';
 import ReactPlayer from 'react-player/youtube';
 import { Link, generatePath, useNavigate } from 'react-router-dom';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import rehypeMathjax from 'rehype-mathjax';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
 import remarkUnwrapImages from 'remark-unwrap-images';
 
 import { trpc } from '@sovereign-academy/api-client';
@@ -152,45 +157,90 @@ export const CourseChapter = () => {
                 </div>
               )}
             </div>
-            <div className="mt-8 w-full px-5 md:mt-16 md:max-w-3xl md:px-0">
-              <h2 className="text-primary-800 mb-5 text-2xl font-normal uppercase italic md:text-3xl">
+            <div className="text-primary-900 mt-8 w-full space-y-6 px-5 md:mt-16 md:max-w-3xl md:px-0">
+              <h2 className="text-primary-800 text-2xl font-normal uppercase italic md:text-3xl">
                 {chapter?.title}
               </h2>
               <ReactMarkdown
                 children={chapter?.raw_content}
                 components={{
                   h2: ({ children }) => (
-                    <h2 className="text-primary-900 mb-5 mt-6 text-xl font-semibold md:mt-10 md:text-2xl">
+                    <h2 className="mt-6 text-xl font-semibold md:mt-10 md:text-2xl">
                       {children}
                     </h2>
                   ),
                   h3: ({ children }) => (
-                    <h3 className="text-primary-900 mb-5 text-xl font-normal">
-                      {children}
-                    </h3>
+                    <h3 className="text-xl font-normal">{children}</h3>
                   ),
                   p: ({ children }) => (
-                    <p className="text-primary-900 mb-5 flex text-justify indent-6 text-base tracking-wide ">
+                    <p className=" text-justify text-base tracking-wide ">
                       {children}
                     </p>
+                  ),
+                  ol: ({ children }) => (
+                    <ol className="flex list-decimal flex-col px-10 text-justify text-base tracking-wide">
+                      {children}
+                    </ol>
+                  ),
+                  ul: ({ children }) => (
+                    <ul className="flex list-disc flex-col px-10 text-justify text-base tracking-wide">
+                      {children}
+                    </ul>
+                  ),
+                  li: ({ children }) => (
+                    <li className="my-1 text-justify text-base tracking-wide last:mb-0">
+                      {children}
+                    </li>
+                  ),
+                  table: ({ children }) => (
+                    <table className="border-primary-900  w-full border-collapse border">
+                      {children}
+                    </table>
+                  ),
+                  th: ({ children }) => (
+                    <th className="border-primary-900 border px-2 py-1">
+                      {children}
+                    </th>
+                  ),
+                  td: ({ children }) => (
+                    <td className="border-primary-900 border px-2 py-1">
+                      {children}
+                    </td>
                   ),
                   img: ({ src, alt }) =>
                     src?.includes('youtube.com') ||
                     src?.includes('youtu.be') ? (
                       <ReactPlayer
-                        className="mx-auto my-12 flex max-w-full	justify-center rounded-lg"
+                        className="mx-auto flex max-w-full justify-center rounded-lg py-6"
                         controls={true}
                         url={src}
                       />
                     ) : (
                       <img
-                        className="mx-auto my-12 flex justify-center rounded-lg"
+                        className="mx-auto flex justify-center rounded-lg py-6"
                         src={src}
                         alt={alt}
                       />
                     ),
+                  code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || '');
+                    return !inline && match ? (
+                      <SyntaxHighlighter
+                        {...props}
+                        children={String(children).replace(/\n$/, '')}
+                        style={dark}
+                        language={match[1]}
+                        PreTag="div"
+                      />
+                    ) : (
+                      <code {...props} className={className}>
+                        {children}
+                      </code>
+                    );
+                  },
                 }}
-                remarkPlugins={[remarkUnwrapImages]}
+                remarkPlugins={[remarkGfm, remarkUnwrapImages, remarkMath]}
+                rehypePlugins={[rehypeMathjax]}
                 transformImageUri={(src) =>
                   uriTransformer(
                     src.startsWith('http')
@@ -204,7 +254,7 @@ export const CourseChapter = () => {
               />
               {chapter.chapter !== chapter.course?.chapters?.length ? (
                 <Link
-                  className="mt-16 flex w-full justify-end"
+                  className="flex w-full justify-end pt-10"
                   to={generatePath(Routes.CourseChapter, {
                     courseId,
                     chapterIndex: (chapter.chapter + 1).toString(),
@@ -217,7 +267,7 @@ export const CourseChapter = () => {
                 </Link>
               ) : (
                 <Link
-                  className="mt-16 flex w-full justify-end"
+                  className="flex w-full justify-end pt-10"
                   to={generatePath(Routes.Course, {
                     courseId,
                   })}
