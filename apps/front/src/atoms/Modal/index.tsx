9e9733ef6406d030cc8b935472b5cfcda27b8666
guillaumehussong <planblockchain@gmail.com>
2023-07-28 18:50:15 +0200

    Rabbit on auth sub-modal

--- apps/front/src/atoms/Modal/index.tsx
+++ apps/front/src/atoms/Modal/index.tsx
@@ -52,26 +52,26 @@ export const Modal = ({
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel>
-                <div className="relative">
-                  <div className="overflow-hidden bg-white rounded-[1.5em]  border-4 border-orange-800 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:max-w-lg sm:p-6">
-                    {' '}
-                    {/*   <button>
+                <div className="overflow-hidden bg-white rounded-[1.5em]  border-4 border-orange-800 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:max-w-lg sm:p-6">
+                  {' '}
+                  {/*   <button>
                     <IoMdClose
                       className="flex h-6 w-6 items-center justify-between"
                       onClick={onClose}
                     />
                   </button> */}
-                    <header className="my-6 flex h-6 flex-col items-center justify-between uppercase text-gray-400 text-xl font-medium">
-                      {headerText && <h4>{headerText}</h4>}
-                    </header>
-                    {children}
-                  </div>
+                  <header className="my-6 flex h-6 flex-col items-center justify-between uppercase text-gray-400 text-xl font-medium">
+                    {headerText && <h4>{headerText}</h4>}
+                  </header>
+                  {children}
+                </div>
+                <div className="relative">
                   <img
                     src={rabbitHikingModal}
                     alt={t('imagesAlt.rabbitHikingModal')}
-                    className="absolute h-m sm:h-20 sm:bottom-23 z-[+1]"
+                    className="absolute hidden sm:h-m h-23 sm:bottom-14 sm:-left-10 z-[+1] sm:flex"
                   ></img>
-                  <div className="relative rounded-[1.5em] justify-center bg-secondary-400 text-white border-4 overflow-hidden shadow-xl transition-all sm:my-8 sm:max-w-lg sm:p-10">
+                  <div className="relative rounded-[1em] justify-center bg-secondary-400 border-orange-800 text-white border-4 overflow-hidden shadow-xl transition-all my-10 sm:max-w-lg p-7  sm:rounded-[1.5em]">
                     <div>{t('auth.noAccountNeeded')}</div>
                   </div>
                 </div>
