4011d79c4b7fef2b0d9cae6b042f469131c53be5
guillaumehussong <planblockchain@gmail.com>
2023-08-10 18:22:00 +0200

    WIP Auth Modal

--- apps/front/src/atoms/Modal/index.tsx
+++ apps/front/src/atoms/Modal/index.tsx
@@ -37,7 +37,7 @@ export const Modal = ({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500/75 transition-opacity" />
+          <div className="fixed inset-0 bg-gray-500/95 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -52,7 +52,7 @@ export const Modal = ({
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel>
-                <div className="overflow-hidden bg-white rounded-[1.5em]  border-4 border-orange-800 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:max-w-lg sm:p-6">
+                <div className="overflow-hidden bg-white rounded-[1.5em]  border-4 border-orange-800 px-4 pb-4 pt-5 text-left shadow-xl transition-all max-w-xs sm:mx-0 sm:my-8 sm:max-w-lg sm:p-6">
                   {' '}
                   {/*   <button>
                     <IoMdClose
@@ -65,7 +65,7 @@ export const Modal = ({
                   </header>
                   {children}
                 </div>
-                <div className="relative">
+                <div className="relative max-w-xs sm:max-w-lg">
                   <img
                     src={rabbitHikingModal}
                     alt={t('imagesAlt.rabbitHikingModal')}
