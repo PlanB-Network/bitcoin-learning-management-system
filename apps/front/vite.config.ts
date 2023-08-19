17e48b755affe20f2c2c7409ce242adabe96c948
Lounès Ksouri <dev@louneskmt.com>
2023-08-19 10:24:34 +0200

    fix(chore): remove useless vite aliases

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest" />
 /// <reference types="vite-plugin-svgr/client" />
-import path from 'path';
 
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react';
@@ -10,13 +9,11 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/front',
+
   server: {
     port: 5555,
     open: false,
     host: process.env.DOCKER ? '0.0.0.0' : 'localhost',
-    fs: {
-      allow: ['../../'],
-    },
   },
 
   preview: {
@@ -33,17 +30,5 @@ export default defineConfig({
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
-  resolve: {
-    alias: [
-      {
-        find: '@sovereign-academy/api-client',
-        replacement: path.resolve('packages/api-client'),
-      },
-      {
-        find: '@sovereign-academy/types',
-        replacement: path.resolve('packages/types'),
-      },
-    ],
   },
 });
