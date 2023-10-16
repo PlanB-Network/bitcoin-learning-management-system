ddfb2688deea05f39b45e961457c19433b6c9ed7
Lounès Ksouri <dev@louneskmt.com>
2023-10-16 18:35:03 +0200

    fix: add express types

--- libs/api/server/src/index.ts
+++ libs/api/server/src/index.ts
@@ -1,1 +1,20 @@
+import type {} from 'express-serve-static-core';
+import type {} from 'qs';
+import 'express-session';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      id: string;
+    }
+  }
+}
+
+declare module 'express-session' {
+  interface SessionData {
+    uid?: string;
+  }
+}
+
 export * from './lib';
