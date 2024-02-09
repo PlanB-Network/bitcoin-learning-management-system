fa6c78e56030ee6660aa529a760075fb7308229a
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-02-09 08:44:37 +0100

    fix: tutorials page horizontal scrollbar

--- apps/web/src/features/dashboard/pages/dashboard.tsx
+++ apps/web/src/features/dashboard/pages/dashboard.tsx
@@ -4,6 +4,7 @@ import {
 } from '@react-hooks-library/core';
 import { useNavigate } from '@tanstack/react-router';
 import { useState } from 'react';
+import { AiOutlineBook } from 'react-icons/ai';
 import { BsPersonFill } from 'react-icons/bs';
 import {
   IoGridOutline,
@@ -42,7 +43,7 @@ const MenuItem = ({
 }) => (
   <div
     className={`flex cursor-pointer flex-row items-center justify-center space-x-4 px-4 py-2 ${
-      active ? 'text-blue-800' : 'text-white'
+      active ? 'bg-orange-400' : 'text-white'
     }`}
     onClick={onClick}
   >
@@ -62,92 +63,83 @@ export const Dashboard = () => {
   if (isMobile) {
     return (
       <MainLayout variant="blue" showFooter={false}>
-        <>
-          <div className="p-6">
-            {currentTab === 'dashboard' && <DashboardTabMobile />}
-            {/* {currentTab === 'courses' && <CoursesTab />} */}
-            {currentTab === 'settings' && <SettingsTab />}
+        <div className="p-6">
+          {currentTab === 'dashboard' && <DashboardTabMobile />}
+          {/* {currentTab === 'courses' && <CoursesTab />} */}
+          {currentTab === 'settings' && <SettingsTab />}
+        </div>
+        <div className="fixed bottom-0 z-[10] flex min-h-[68px] w-full items-center justify-around bg-orange-600 md:hidden">
+          <MenuItem
+            text="Dashboard"
+            icon={<IoGridOutline size={28} />}
+            active={currentTab === 'dashboard'}
+            onClick={() => setCurrentTab('dashboard')}
+          />
+          <MenuItem
+            text="Settings"
+            icon={<IoSettingsOutline size={28} />}
+            active={currentTab === 'settings'}
+            onClick={() => setCurrentTab('settings')}
+          />
+          <MenuItem
+            text="Sign Out"
+            icon={<IoLogOutOutline size={28} />}
+            onClick={() => {
+              dispatch(userSlice.actions.logout());
+              navigate({ to: '/' });
+            }}
+          />
+        </div>
+      </MainLayout>
+    );
+  }
+
+  return (
+    <MainLayout>
+      <div className="flex flex-row text-white">
+        {/* Left menu */}
+        <div className=" flex w-64 flex-col gap-8 rounded-3xl p-4">
+          <div className="flex items-center gap-2 rounded-3xl pl-2">
+            <BsPersonFill className="text-blue-1000 h-10 w-10 overflow-hidden rounded-full bg-white" />
+            <p className="text-lg font-medium italic">{user?.username}</p>
           </div>
-          <div className="fixed bottom-0 z-[10] flex min-h-[68px] w-full items-center justify-around bg-orange-600 md:hidden">
-            <MenuItem
-              text="Dashboard"
-              icon={<IoGridOutline size={28} />}
-              active={currentTab === 'dashboard'}
-              onClick={() => setCurrentTab('dashboard')}
-            />
-            <MenuItem
-              text="Settings"
-              icon={<IoSettingsOutline size={28} />}
-              active={currentTab === 'settings'}
-              onClick={() => setCurrentTab('settings')}
-            />
+
+          <div className="flex h-full flex-col items-start justify-between">
+            <div className="flex flex-col items-start justify-center">
+              <MenuItem
+                text="Dashboard"
+                icon={<IoGridOutline size={20} />}
+                active={currentTab === 'dashboard'}
+                onClick={() => setCurrentTab('dashboard')}
+              />
+              <MenuItem
+                text="My Courses"
+                icon={<AiOutlineBook />}
+                active={currentTab === 'courses'}
+                onClick={() => setCurrentTab('courses')}
+              />
+              <MenuItem
+                text="Settings"
+                icon={<IoSettingsOutline size={20} />}
+                active={currentTab === 'settings'}
+                onClick={() => setCurrentTab('settings')}
+              />
+            </div>
             <MenuItem
               text="Sign Out"
-              icon={<IoLogOutOutline size={28} />}
+              icon={<IoLogOutOutline size={20} />}
               onClick={() => {
                 dispatch(userSlice.actions.logout());
                 navigate({ to: '/' });
               }}
             />
           </div>
-        </>
-      </MainLayout>
-    );
-  }
+        </div>
 
-  return (
-    <MainLayout variant="dark" footerVariant="dark">
-      <div className="bg-blue-1000 min-h-screen p-10">
-        <div className="mx-auto grid min-h-[800px] max-w-4xl grid-cols-4 overflow-hidden rounded-3xl bg-gray-200 shadow xl:max-w-5xl">
-          <div className="col-span-1 flex max-w-sm flex-col items-start justify-start space-y-8 bg-orange-500 p-4">
-            <div className="w-full px-2">
-              <div className="flex w-full items-center space-x-2 rounded-3xl bg-orange-400">
-                <div className="p-1">
-                  <div className="h-10 w-10 overflow-hidden rounded-full bg-white">
-                    <BsPersonFill className="h-10 w-10" />
-                  </div>
-                </div>
-                <div className="text-lg font-medium italic text-blue-900">
-                  {user?.username}
-                </div>
-              </div>
-            </div>
-            <div className="flex h-full flex-col items-start justify-between">
-              <div className="flex flex-col items-start justify-center">
-                <MenuItem
-                  text="Dashboard"
-                  icon={<IoGridOutline size={20} />}
-                  active={currentTab === 'dashboard'}
-                  onClick={() => setCurrentTab('dashboard')}
-                />
-                {/* <MenuItem
-                      text="My Courses"
-                      icon={<AiOutlineBook />}
-                      active={currentTab === 'courses'}
-                      onClick={() => setCurrentTab('courses')}
-                    /> */}
-                <MenuItem
-                  text="Settings"
-                  icon={<IoSettingsOutline size={20} />}
-                  active={currentTab === 'settings'}
-                  onClick={() => setCurrentTab('settings')}
-                />
-              </div>
-              <MenuItem
-                text="Sign Out"
-                icon={<IoLogOutOutline size={20} />}
-                onClick={() => {
-                  dispatch(userSlice.actions.logout());
-                  navigate({ to: '/' });
-                }}
-              />
-            </div>
-          </div>
-          <div className="col-span-3 p-10">
-            {currentTab === 'dashboard' && <DashboardTab />}
-            {/* {currentTab === 'courses' && <CoursesTab />} */}
-            {currentTab === 'settings' && <SettingsTab />}
-          </div>
+        <div className="ml-4 grow rounded-3xl bg-gray-200 p-10">
+          {currentTab === 'dashboard' && <DashboardTab />}
+          {/* {currentTab === 'courses' && <CoursesTab />} */}
+          {currentTab === 'settings' && <SettingsTab />}
         </div>
       </div>
     </MainLayout>
