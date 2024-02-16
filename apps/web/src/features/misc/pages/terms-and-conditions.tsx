ab88c07cd4ade7047d1b4c74afa836286580640b
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-02-16 09:49:41 +0100

    feat: add terms and conditions

--- apps/web/src/features/misc/pages/terms-and-conditions.tsx
+++ apps/web/src/features/misc/pages/terms-and-conditions.tsx
@@ -1,15 +1,31 @@
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
+import { Button } from '../../../atoms/Button';
 import { MainLayout } from '../../../components/MainLayout';
 
 export const TermsAndConditions = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <MainLayout footerVariant="dark">
-      <h1 className="m-auto flex justify-center text-white">
-        Terms and conditions
-      </h1>
+      <div className="flex flex-col items-center px-4">
+        <div className="mx-8 mt-24 flex max-w-4xl flex-col items-center">
+          <h1 className="text-4xl font-semibold uppercase text-orange-500">
+            {t('words.termsAndConditions')}
+          </h1>
+        </div>
+
+        <a
+          href="/src/assets/terms.pdf"
+          target="_blank"
+          download
+          rel="noreferrer"
+        >
+          <Button variant="tertiary" className="mt-6">
+            {t('words.download')}
+          </Button>
+        </a>
+      </div>
     </MainLayout>
   );
 };
