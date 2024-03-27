e7e1e2f52e2f944430d8b60557ea44ae90d2d60c
Kevin Mulier <kevin.mulier@outlook.fr>
2024-03-27 13:50:02 +0100

    feat: add events page introduction

---
+++ apps/web/src/features/events/components/events-introduction.tsx
@@ -0,0 +1,22 @@
+export const EventsIntroduction = () => {
+  return (
+    <section className="text-center max-w-4xl mx-auto">
+      <h1 className="text-xs mb-5 sm:text-base sm:mb-10">
+        WELCOME TO PLAN B NETWORK EVENTS
+      </h1>
+      <p className="text-sm tracking-tight text-newOrange-1 font-semibold mb-2 sm:text-xl">
+        Bottom up adoption enhanced by a strong network
+      </p>
+      <p className="text-2xl mb-6 sm:text-[40px]">
+        Looking for a Bitcoin event to go to ?
+      </p>
+      <p className="text-sm text-newGray-1 sm:text-base">
+        Want to talk about Bitcoin in real time ? Here you can find all the
+        events supported by the Plan B Network communities. Whether you want to
+        listen to a lecture, discuss Bitcoin around a drink, or take an exam to
+        assess your knowledge, you’ll be able to find the right event here
+        through the Plan B Network.
+      </p>
+    </section>
+  );
+};
