24fbf8fe0ca29ebbf9c66006861ab574e5b6c7a9
Kevin Mulier <kevin.mulier@outlook.fr>
2024-03-27 10:46:19 +0100

    feat: add title for upcoming events

--- apps/web/src/features/events/components/events-grid.tsx
+++ apps/web/src/features/events/components/events-grid.tsx
@@ -1,14 +1,24 @@
+import { useTranslation } from 'react-i18next';
+
 import { EventCard } from './event-card.tsx';
 
 export const EventsGrid = () => {
+  const { t } = useTranslation();
+
   return (
-    <div className="flex flex-wrap justify-center gap-5 md:gap-[30px] mt-6 md:mt-12 mx-auto">
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-    </div>
+    <>
+      <h2 className="text-xl font-medium mt-4">
+        {t('events.main.upcomingEvents')}
+      </h2>
+
+      <div className="flex flex-wrap justify-center gap-5 md:gap-[30px] mt-6 md:mt-12 mx-auto">
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+      </div>
+    </>
   );
 };
