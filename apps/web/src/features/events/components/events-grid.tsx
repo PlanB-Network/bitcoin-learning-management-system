ef41264344598d625317d3d3a9103c96dd6f6938
Kevin Mulier <kevin.mulier@outlook.fr>
2024-03-27 13:49:03 +0100

    feat: improve display

--- apps/web/src/features/events/components/events-grid.tsx
+++ apps/web/src/features/events/components/events-grid.tsx
@@ -6,11 +6,8 @@ export const EventsGrid = () => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <h2 className="text-xl font-medium mt-4">
-        {t('events.main.upcomingEvents')}
-      </h2>
-
+    <div className="flex flex-col">
+      <h2 className="text-xl font-medium">{t('events.main.upcomingEvents')}</h2>
       <div className="flex flex-wrap justify-center gap-5 md:gap-[30px] mt-6 md:mt-12 mx-auto">
         <EventCard />
         <EventCard />
@@ -19,6 +16,6 @@ export const EventsGrid = () => {
         <EventCard />
         <EventCard />
       </div>
-    </>
+    </div>
   );
 };
