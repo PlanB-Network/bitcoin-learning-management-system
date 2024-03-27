c88edc71b065510a42a0c3b82f18322e2880a03a
Kevin Mulier <kevin.mulier@outlook.fr>
2024-03-27 13:58:31 +0100

    feat: add translation

--- apps/web/src/features/events/components/events-introduction.tsx
+++ apps/web/src/features/events/components/events-introduction.tsx
@@ -1,21 +1,21 @@
+import { useTranslation } from 'react-i18next';
+
 export const EventsIntroduction = () => {
+  const { t } = useTranslation();
+
   return (
     <section className="text-center max-w-4xl mx-auto">
-      <h1 className="text-xs mb-5 sm:text-base sm:mb-10">
-        WELCOME TO PLAN B NETWORK EVENTS
+      <h1 className="text-xs uppercase mb-5 sm:text-base sm:mb-10">
+        {t('events.introduction.introductionh1')}
       </h1>
       <p className="text-sm tracking-tight text-newOrange-1 font-semibold mb-2 sm:text-xl">
-        Bottom up adoption enhanced by a strong network
+        {t('events.introduction.introductionp1')}
       </p>
       <p className="text-2xl mb-6 sm:text-[40px]">
-        Looking for a Bitcoin event to go to ?
+        {t('events.introduction.introductionp2')}
       </p>
       <p className="text-sm text-newGray-1 sm:text-base">
-        Want to talk about Bitcoin in real time ? Here you can find all the
-        events supported by the Plan B Network communities. Whether you want to
-        listen to a lecture, discuss Bitcoin around a drink, or take an exam to
-        assess your knowledge, you’ll be able to find the right event here
-        through the Plan B Network.
+        {t('events.introduction.introductionp3')}
       </p>
     </section>
   );
