795360bc4762d1a51eaccd2f42a4be5aabf494d2
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-03-28 11:41:26 +0100

    feat: add default value for path

--- packages/database/drizzle/0008_serious_marvel_zombies.sql
+++ packages/database/drizzle/0008_serious_marvel_zombies.sql
@@ -4,7 +4,7 @@ ALTER TABLE "users"."event_payment" DROP CONSTRAINT "event_payment_event_id_even
 --> statement-breakpoint
 ALTER TABLE "content"."event_languages" ALTER COLUMN "event_id" SET DATA TYPE varchar(100);--> statement-breakpoint
 ALTER TABLE "content"."event_tags" ALTER COLUMN "event_id" SET DATA TYPE varchar(100);--> statement-breakpoint
-ALTER TABLE "content"."events" ADD COLUMN "path" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "content"."events" ADD COLUMN "path" varchar(255) NOT NULL DEFAULT '#';--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "users"."event_payment" ADD CONSTRAINT "event_payment_event_id_events_id_fk" FOREIGN KEY ("event_id") REFERENCES "content"."events"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
