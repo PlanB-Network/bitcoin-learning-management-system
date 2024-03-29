3036b93c25341222356dea5e309b2acbb8b614da
Kevin Mulier <kevin.mulier@outlook.fr>
2024-03-29 12:31:36 +0100

    feat: change font weight

--- apps/web/src/features/events/components/events-introduction.tsx
+++ apps/web/src/features/events/components/events-introduction.tsx
@@ -8,7 +8,7 @@ export const EventsIntroduction = () => {
       <h1 className="text-xs uppercase mb-5 md:text-base md:mb-10">
         {t('events.introduction.introductionh1')}
       </h1>
-      <p className="text-sm tracking-[-3%] text-newOrange-1 font-semibold mb-2 md:text-xl md:tracking-normal">
+      <p className="text-sm tracking-[-3%] text-newOrange-1 font-medium mb-2 md:text-xl md:tracking-normal">
         {t('events.introduction.introductionp1')}
       </p>
       <p className="text-2xl mb-6 md:text-[40px] md:tracking-[0.25px]">
