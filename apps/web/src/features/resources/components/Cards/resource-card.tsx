93f81d7551de7d12115447c0efbd657c375e4191
Kevin Mulier <kevin.mulier@outlook.fr>
2024-04-05 10:32:13 +0200

    feat: add new colors to resource cards

--- apps/web/src/features/resources/components/Cards/resource-card.tsx
+++ apps/web/src/features/resources/components/Cards/resource-card.tsx
@@ -7,22 +7,22 @@ interface ResourceCardProps {
 
 export const ResourceCard = (props: ResourceCardProps) => {
   return (
-    <div className="group flex md:flex-col gap-4 p-3 bg-newBlack-2 md:bg-transparent hover:bg-newBlack-3 md:hover:bg-newOrange-1 max-md:hover:shadow-[0px_1px_1px_0px_rgba(255,92,0,0.67)]  rounded-2xl md:hover:z-10 md:hover:scale-110 md:transition-all md:duration-300">
+    <div className="group flex md:flex-col gap-4 p-3  hover:bg-darkOrange-10 hover:shadow-[0px_0px_10px_0px_#FF5C00] hover:border border-transparent hover:border-darkOrange-6 rounded-2xl md:hover:z-10 md:hover:scale-110 transition-all duration-300">
       <img
         className="aspect-square object-contain w-[84px] md:w-full md:group-hover:rounded-2xl transition-all duration-300"
         src={props.imageSrc ? props.imageSrc : ''}
         alt={props.name}
       />
       <div className="flex flex-col gap-[10px] md:gap-4 ">
-        <span className="text-white max-md:group-hover:text-newOrange-1 text-sm md:text-xl md:leading-5 font-medium">
+        <span className="text-white text-sm md:text-xl md:leading-5 font-medium">
           {props.name}
         </span>
-        <span className="text-white max-md:group-hover:text-newOrange-1 text-xs md:text-base md:leading-6">
+        <span className="text-white group-hover:text-darkOrange-1 text-xs md:text-base md:leading-6 transition-all">
           {props.author}
           {props.year && (
             <>
               <span> · </span>
-              <span className="text-white/75 max-md:group-hover:text-newOrange-1 font-light">
+              <span className="text-white/75 group-hover:text-darkOrange-1 font-light transition-all">
                 {props.year}
               </span>
             </>
