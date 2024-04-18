bd1113ddec68236a6d67568a6d1850ae2f95143b
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-04-18 09:18:57 +0200

    refactor: ticket generation

--- packages/user/src/lib/courses/services/generate-chapter-ticket.ts
+++ packages/user/src/lib/courses/services/generate-chapter-ticket.ts
@@ -7,31 +7,6 @@ const doc = new PDFDocument();
 let x = 0;
 let y = 0;
 
-function print(content: string | null | undefined) {
-  if (content) {
-    doc.text(content, x, y);
-  }
-}
-
-function printAndMove(content: string | null | undefined, moveY?: number) {
-  if (content) {
-    doc.text(content, x, y);
-    y += moveY ? moveY : 18;
-  }
-}
-
-function addImage(path: string, x: number, y: number, w: number) {
-  try {
-    if (fs.existsSync(path)) {
-      doc.image(path, x, y, { width: w });
-    } else {
-      console.warn('Logo file not found, skipping logo insertion.');
-    }
-  } catch (error) {
-    console.error('Failed to add image:', error);
-  }
-}
-
 export const generateChapterTicket = async ({
   title,
   addressLine1,
@@ -68,13 +43,30 @@ export const generateChapterTicket = async ({
 
     doc.on('error', reject);
 
+    printStaticElements();
+    printDescription();
+    printContact();
+    printTerms();
+    printRightCard();
+
+    doc.end();
+  });
+
+  function printStaticElements() {
     addImage('./src/assets/pdf-header.png', 0, 0, 615);
     addImage('./src/assets/card.png', 380, 175, 200);
 
+    doc.fontSize(24).fillColor('darkorange').font('Helvetica-Bold');
+    x = 130;
+    y = doc.page.height - 110;
+    doc.text('We wish you a great time !', x, y, { continued: false });
+  }
+
+  function printDescription() {
     x = 20;
     y = 180;
 
-    doc.fontSize(20).font('Helvetica-Bold');
+    doc.fontSize(20).fillColor('black').font('Helvetica-Bold');
     print(title);
     y += 30;
 
@@ -109,7 +101,9 @@ export const generateChapterTicket = async ({
       .moveTo(x, y)
       .lineTo(x + 200, y)
       .stroke();
+  }
 
+  function printContact() {
     y += 20;
     doc.fontSize(18).fillColor('black').font('Helvetica-Bold');
     printAndMove('Contact', 30);
@@ -117,7 +111,9 @@ export const generateChapterTicket = async ({
     doc.fontSize(16).font('Helvetica');
     x = 25;
     printAndMove(contact);
+  }
 
+  function printTerms() {
     x = 20;
     y += 20;
     doc.fontSize(18).font('Helvetica-Bold');
@@ -140,12 +136,9 @@ export const generateChapterTicket = async ({
     printAndMove('• It must not be duplicated or photocopied. Any', 15);
     x = 36;
     printAndMove('reproduction is fraudulent and unnecessary.', 15);
+  }
 
-    doc.fontSize(24).fillColor('darkorange').font('Helvetica-Bold');
-    x = 130;
-    y = doc.page.height - 110;
-    doc.text('We wish you a great time !', x, y, { continued: false });
-
+  function printRightCard() {
     x = 400;
     y = 190;
     doc.fontSize(12).fillColor('black').font('Helvetica-Bold');
@@ -161,7 +154,30 @@ export const generateChapterTicket = async ({
     // eslint-disable-next-line import/no-named-as-default-member
     const qrBuffer = qr.imageSync(`displayName:${userDisplayName}`);
     doc.image(qrBuffer, x, y, { width: 170 });
+  }
 
-    doc.end();
-  });
+  function print(content: string | null | undefined) {
+    if (content) {
+      doc.text(content, x, y);
+    }
+  }
+
+  function printAndMove(content: string | null | undefined, moveY?: number) {
+    if (content) {
+      doc.text(content, x, y);
+      y += moveY ? moveY : 18;
+    }
+  }
+
+  function addImage(path: string, x: number, y: number, w: number) {
+    try {
+      if (fs.existsSync(path)) {
+        doc.image(path, x, y, { width: w });
+      } else {
+        console.warn('Logo file not found, skipping logo insertion.');
+      }
+    } catch (error) {
+      console.error('Failed to add image:', error);
+    }
+  }
 };
