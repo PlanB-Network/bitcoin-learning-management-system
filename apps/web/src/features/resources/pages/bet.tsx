654072780ebc0066b3253b61aff667cb326da7c2
Kevin Mulier <kevin.mulier@outlook.fr>
2024-05-16 17:29:12 +0200

    feat: redesign BET

--- apps/web/src/features/resources/pages/bet.tsx
+++ apps/web/src/features/resources/pages/bet.tsx
@@ -1,6 +1,12 @@
+import { t } from 'i18next';
 import { useTranslation } from 'react-i18next';
+import { FiDownload } from 'react-icons/fi';
+import { IoIosSearch } from 'react-icons/io';
 
-import { cn } from '@sovereign-university/ui';
+import { Button } from '@sovereign-university/ui';
+
+import type { VerticalCardProps } from '#src/molecules/VerticalCard/index.js';
+import { VerticalCard } from '#src/molecules/VerticalCard/index.js';
 
 import poster from '../../../assets/resources/bet/21poster.webp';
 import anil from '../../../assets/resources/bet/anil.webp';
@@ -18,62 +24,29 @@ import propaganda from '../../../assets/resources/bet/propaganda.webp';
 import scams from '../../../assets/resources/bet/scams.webp';
 import { ResourceLayout } from '../layout.tsx';
 
-const LinkBox = ({
-  title,
-  description,
-  figmaUrl,
-  imgFig,
-  cardColor,
-  hoverColor,
-}: {
-  title: string;
-  description: string;
-  figmaUrl: string;
-  imgFig: string;
-  cardColor: string;
-  hoverColor: string;
-}) => {
-  return (
-    <a href={figmaUrl} target="_blank" rel="noopener noreferrer">
-      <div
-        className={cn(
-          'shrink-0 h-full rounded-lg bg-red-600 p-3 shadow-md hover:bg-blue-600 sm:p-4',
-          cardColor,
-          hoverColor,
-        )}
-      >
-        <img
-          src={imgFig}
-          alt={title}
-          className="mb-4 h-32 w-full object-cover"
-        />
-        <h3 className="mb-2 font-semibold">{title}</h3>
-        <p className="text-xs sm:text-sm">{description}</p>
-      </div>
-    </a>
-  );
-};
-
 const Section = ({ children }: { children: React.ReactNode }) => {
-  return <div className="space-y-2">{children}</div>;
+  return <div className="flex flex-col">{children}</div>;
 };
 
 const SectionTitle = ({ children }: { children: string }) => {
   return (
-    <h2 className="text-xl font-normal underline decoration-orange-600 decoration-[3px] underline-offset-[5px] sm:text-2xl">
+    <h2 className="mobile-h3 max-md:text-darkOrange-5 md:desktop-h3 md:text-center mb-2">
       {children}
     </h2>
   );
 };
 
 const SectionDescription = ({ children }: { children: string }) => {
-  return <p className="text-xs font-light italic sm:text-base">{children}</p>;
+  return (
+    <p className="max-md:hidden text-center mb-2 md:mb-12 desktop-body1">
+      {children}
+    </p>
+  );
 };
 
 const SectionGrid = ({
   elements,
   cardColor,
-  hoverColor,
 }: {
   elements: Array<{
     title: string;
@@ -81,21 +54,31 @@ const SectionGrid = ({
     figmaUrl: string;
     imgFig: string;
   }>;
-  cardColor: string;
-  hoverColor: string;
+  cardColor: VerticalCardProps['cardColor'];
 }) => {
   return (
-    <div className="overflow-x-auto py-6">
-      <div className="grid grid-cols-2 gap-4 sm:w-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
-        {elements.map((item, index) => (
-          <LinkBox
-            key={index}
-            {...item}
-            cardColor={cardColor}
-            hoverColor={hoverColor}
-          />
-        ))}
-      </div>
+    <div className="grid grid-cols-2 gap-4 sm:w-auto sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {elements.map((item, index) => (
+        <VerticalCard
+          key={index}
+          title={item.title}
+          subtitle={item.description}
+          imageSrc={item.imgFig}
+          languages={[]}
+          link={item.figmaUrl}
+          buttonText={t('words.view')}
+          buttonIcon={<IoIosSearch />}
+          buttonVariant="newPrimary"
+          secondaryLink={item.figmaUrl}
+          secondaryButtonText={t('words.download')}
+          secondaryButtonIcon={<FiDownload />}
+          secondaryButtonVariant="newSecondary"
+          externalLink
+          onHoverArrow={false}
+          cardColor={cardColor}
+          onHoverCardColorChange
+        />
+      ))}
     </div>
   );
 };
@@ -213,42 +196,61 @@ export const BET = () => {
       title={t('bet.pageTitle')}
       tagLine={t('bet.pageSubtitle')}
       activeCategory="bet"
+      marginTopChildren={false}
+      maxWidth="1360"
+      hidePageHeaderMobile
     >
-      <div className="space-y-8 px-4 text-gray-100 sm:px-8">
-        <div className="max-w-3xl space-y-4">
-          <p className="max-w-3xl">{t('bet.pageDescription')}</p>
-          <h1 className="font-semibold ">{t('bet.hereForYou')}</h1>
+      <div className="flex flex-col text-newGray-5 mt-8 max-md:mx-2">
+        <p className="max-w-4xl mx-auto text-center mobile-subtitle2 md:desktop-h7 font-medium max-md:mb-6">
+          {t('bet.pageDescription')}
+        </p>
+
+        <div className="h-px max-w-6xl w-full bg-newGray-1 max-md:hidden my-16 mx-auto" />
+
+        <div className="flex flex-col max-md:gap-7">
+          {/* Section of Educational Content */}
+          <Section>
+            <SectionTitle>{t('bet.educationalContent.title')}</SectionTitle>
+            <SectionDescription>
+              {t('bet.educationalContent.description')}
+            </SectionDescription>
+            <SectionGrid elements={EDUCATIONAL_CONTENT} cardColor="orange" />
+          </Section>
+
+          <div className="h-px max-w-6xl w-full bg-newGray-1 max-md:hidden my-16 mx-auto" />
+
+          {/* Section of Visual Content */}
+          <Section>
+            <SectionTitle>{t('bet.visualContent.title')}</SectionTitle>
+            <SectionDescription>
+              {t('bet.visualContent.description')}
+            </SectionDescription>
+            <SectionGrid elements={VISUAL_CONTENT} cardColor="maroon" />
+          </Section>
         </div>
 
-        {/* Section of Educational Content */}
-        <Section>
-          <SectionTitle>{t('bet.educationalContent.title')}</SectionTitle>
-          <SectionDescription>
-            {t('bet.educationalContent.description')}
-          </SectionDescription>
-          <SectionGrid
-            elements={EDUCATIONAL_CONTENT}
-            cardColor="bg-red-600"
-            hoverColor="hover:bg-blue-600"
-          />
-        </Section>
+        <div className="h-px max-w-6xl w-full bg-newGray-1 max-md:hidden my-16 mx-auto" />
 
-        {/* Section of Visual Content */}
-        <Section>
-          <SectionTitle>{t('bet.visualContent.title')}</SectionTitle>
-          <SectionDescription>
-            {t('bet.visualContent.description')}
-          </SectionDescription>
-          <SectionGrid
-            elements={VISUAL_CONTENT}
-            cardColor="bg-green-600"
-            hoverColor="hover:bg-blue-500"
-          />
-        </Section>
-
-        <p className="border-y-2 px-8 py-5 text-center uppercase">
-          {t('bet.contribute')}
-        </p>
+        <div className="max-md:mt-7 w-full max-w-4xl mx-auto border border-newBlack-5 rounded-[1.25rem] px-2.5 py-4 md:p-5">
+          <h3 className="desktop-h6 max-md:text-darkOrange-5 text-[40px] font-normal leading-tight tracking-[0.25px] max-md:text-center mb-2.5">
+            {t('bet.contributeTitle')}
+          </h3>
+          <div className="flex items-center justify-center max-md:flex-col md:gap-10">
+            <p className="max-md:text-center desktop-typo1 md:text-xl md:leading-snug max-md:mb-2.5 md:max-w-[541px]">
+              {t('bet.contributeDescription')}
+            </p>
+            <a
+              href="https://github.com/DecouvreBitcoin/sovereign-university-data"
+              target="_blank"
+              className="max-md:mx-auto md:ml-auto shrink-0"
+              rel="noreferrer"
+            >
+              <Button variant="newPrimary" onHoverArrow size="l">
+                {t('bet.contributeButton')}
+              </Button>
+            </a>
+          </div>
+        </div>
       </div>
     </ResourceLayout>
   );
