47f03c3c2ba8525d4e9d832da06df2cb9d59bbff
max-lt <maxime.tissier@gmail.com>
2024-05-17 17:16:53 +0200

    fix: fix slow app compilation

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,15 +1,12 @@
-import { join } from 'node:path';
-
 import type { Config } from 'tailwindcss';
 
 import baseConfig from '../../packages/ui/tailwind.config-base.js';
 
 export default {
   content: [
-    join(
-      '{src,pages,components,app,atoms}/**/*!(*.stories|*.spec).{ts,tsx,html}',
-    ),
-    '../../packages/ui/**/*.{js,jsx,ts,tsx}',
+    './src/**/*!(*.stories|*.spec).{ts,tsx,html}',
+    '../../packages/ui/src/**/*.{ts,tsx}',
+    '../../packages/ui/storybook/**/*.{ts,tsx}',
   ],
   presets: [baseConfig],
 } satisfies Config;
