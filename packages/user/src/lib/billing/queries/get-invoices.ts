efce1ac3afc1f152167e3e1d7b538c586e160e29
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-05-27 17:02:10 +0200

    feat: add course name in billing tab

--- packages/user/src/lib/billing/queries/get-invoices.ts
+++ packages/user/src/lib/billing/queries/get-invoices.ts
@@ -1,7 +1,7 @@
 import { sql } from '@sovereign-university/database';
 import type { Invoice } from '@sovereign-university/types';
 
-export const getInvoicesQuery = (uid: string) => {
+export const getInvoicesQuery = (uid: string, language?: string) => {
   return sql<Invoice[]>`
     SELECT ep.last_updated as date , ce.name as title, ce.type::text as type
     FROM  users.event_payment ep
@@ -10,9 +10,18 @@ export const getInvoicesQuery = (uid: string) => {
     
     UNION ALL
 
-    SELECT cp.last_updated as date, '-' as title, 'course' as type
+    SELECT cp.last_updated as date, cl.name as title, 'course' as type
     FROM users.course_payment cp
     JOIN content.courses c ON cp.course_id = c.id
-    WHERE cp.uid = ${uid};
+    JOIN content.courses_localized cl ON c.id = cl.course_id
+    WHERE cp.uid = ${uid}
+    ${language ? sql`AND cl.language = ${language}` : sql``}
+
+    GROUP BY 
+      c.id, 
+      cl.language,
+      cl.name,
+      cp.last_updated
+    ;
   `;
 };
