5e0f8ccc035147f4e754a8a7ce7dd5204c22b3f8
max-lt <maxime.tissier@gmail.com>
2024-06-04 11:55:21 +0200

    Fix dynamic links

--- apps/web/src/utils/index.ts
+++ apps/web/src/utils/index.ts
@@ -1,13 +1,7 @@
-import { getDomain, isDevelopmentEnvironment } from './misc.ts';
-
 export { trpc } from './trpc.ts';
 
 export const computeAssetCdnUrl = (commitHash: string, path: string) => {
-  const baseUrl = isDevelopmentEnvironment()
-    ? 'http://localhost:8080'
-    : `https://cdn.${getDomain()}`;
-
-  return `${baseUrl}/${commitHash}/${path}`;
+  return `/cdn/${commitHash}/${path}`;
 };
 
 export const compose = (...args: string[]) => args.join(' ');
