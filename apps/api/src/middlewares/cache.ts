3088c6a8de176b5860336e2680d24ca4d99912dd
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-06-20 13:04:57 +0200

    fix: temporarily disable redis cache to fix date issue

--- apps/api/src/middlewares/cache.ts
+++ apps/api/src/middlewares/cache.ts
@@ -3,73 +3,73 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import crypto from 'node:crypto';
+// import crypto from 'node:crypto';
 
 import { createMiddleware } from '../trpc/index.js';
 
 // Helper function to sort an object
-const sortObject = (obj: any): any => {
-  if (obj === null) return null;
-  if (typeof obj !== 'object') return obj;
-  if (Array.isArray(obj)) return obj.map((element) => sortObject(element));
+// const sortObject = (obj: any): any => {
+//   if (obj === null) return null;
+//   if (typeof obj !== 'object') return obj;
+//   if (Array.isArray(obj)) return obj.map((element) => sortObject(element));
 
-  return (
-    Object.keys(obj)
-      .sort()
-      // eslint-disable-next-line unicorn/no-array-reduce
-      .reduce((result: { [key: string]: any }, key) => {
-        result[key] = sortObject(obj[key]);
-        return result;
-      }, {})
-  );
-};
+//   return (
+//     Object.keys(obj)
+//       .sort()
+//       // eslint-disable-next-line unicorn/no-array-reduce
+//       .reduce((result: { [key: string]: any }, key) => {
+//         result[key] = sortObject(obj[key]);
+//         return result;
+//       }, {})
+//   );
+// };
 
-const hashObject = (obj: any): string => {
-  const sortedObj = sortObject(obj);
-  const str = JSON.stringify(sortedObj);
+// const hashObject = (obj: any): string => {
+//   const sortedObj = sortObject(obj);
+//   const str = JSON.stringify(sortedObj);
 
-  return crypto.createHash('sha256').update(str).digest('hex');
-};
+//   return crypto.createHash('sha256').update(str).digest('hex');
+// };
 
-export const cacheMiddleware = createMiddleware(
-  async ({ ctx, next, path, type, getRawInput }) => {
-    const {
-      dependencies: { redis },
-    } = ctx;
+export const cacheMiddleware = createMiddleware(async ({ next }) => {
+  // async ({ ctx, next, path, type, getRawInput }) => {
+  // const {
+  //   dependencies: { redis },
+  // } = ctx;
 
-    if (
-      type === 'query' &&
-      path.startsWith('content.') &&
-      !/coupon/i.test(path)
-    ) {
-      const rawInput = await getRawInput();
+  // Temporary disable redis cache
+  // if (
+  //   type === 'query' &&
+  //   path.startsWith('content.') &&
+  //   !/coupon/i.test(path)
+  // ) {
+  //   const rawInput = await getRawInput();
 
-      const inputHash = rawInput ? hashObject(rawInput) : 'no-input';
-      const key = `trpc:${path}:${inputHash}` as const;
+  //   const inputHash = rawInput ? hashObject(rawInput) : 'no-input';
+  //   const key = `trpc:${path}:${inputHash}` as const;
 
-      const cached = await redis.get(key);
+  //   const cached = await redis.get(key);
 
-      if (cached && process.env.NODE_ENV !== 'development') {
-        console.log(`Cache hit for ${key}`);
-        return {
-          marker: 'middlewareMarker' as 'middlewareMarker' & {
-            __brand: 'middlewareMarker';
-          },
-          ok: true,
-          data: cached,
-          ctx,
-        };
-      }
+  //   if (cached && process.env.NODE_ENV !== 'development') {
+  //     console.log(`Cache hit for ${key}`);
+  //     return {
+  //       marker: 'middlewareMarker' as 'middlewareMarker' & {
+  //         __brand: 'middlewareMarker';
+  //       },
+  //       ok: true,
+  //       data: cached,
+  //       ctx,
+  //     };
+  //   }
 
-      const result = await next();
+  //   const result = await next();
 
-      if (result.ok) {
-        await redis.setWithExpiry(key, result.data, 86_400);
-      }
+  //   if (result.ok) {
+  //     await redis.setWithExpiry(key, result.data, 86_400);
+  //   }
 
-      return result;
-    }
+  //   return result;
+  // }
 
-    return next();
-  },
-);
+  return next();
+});
