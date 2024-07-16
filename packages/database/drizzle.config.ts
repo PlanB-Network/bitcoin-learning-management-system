80eda3a79f7aca84b57e59402d37302eddb37354
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-07-16 20:18:48 +0200

    refactor: move drizzle migration files

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   schema: './drizzle/schema.ts',
-  out: './drizzle',
+  out: './drizzle/migrations',
   dialect: 'postgresql',
   introspect: {
     casing: 'camel',
