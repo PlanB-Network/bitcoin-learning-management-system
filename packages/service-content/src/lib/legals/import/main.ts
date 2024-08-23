1265a7e8b2d587b1602ee379402a76eb9b9ed936
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-08-23 09:49:12 +0200

    fix: legals sync

--- packages/service-content/src/lib/legals/import/main.ts
+++ packages/service-content/src/lib/legals/import/main.ts
@@ -1,14 +1,8 @@
 import type { TransactionSql } from '@blms/database';
 import { firstRow } from '@blms/database';
-import type {
-  ChangedFile,
-  Legal,
-  ModifiedFile,
-  RenamedFile,
-} from '@blms/types';
+import type { Legal, ModifiedFile, RenamedFile } from '@blms/types';
 
 import type { ChangedLegal } from './index.js';
-import { parseDetailsFromPath } from './index.js';
 
 /**
  * Process the main file of a legal document
@@ -18,39 +12,14 @@ import { parseDetailsFromPath } from './index.js';
  * @param file - The changed file
  */
 export const createProcessMainFile =
-  (transaction: TransactionSql) =>
-  async (legal: ChangedLegal, file?: ChangedFile) => {
-    if (!file) return;
+  (transaction: TransactionSql) => async (legal: ChangedLegal) => {
+    const lastUpdated = legal.files
+      .filter(
+        (file): file is ModifiedFile | RenamedFile => file.kind !== 'removed',
+      )
+      .sort((a, b) => b.time - a.time)[0];
 
-    if (file.kind === 'removed') {
-      await transaction`
-        DELETE FROM content.legals WHERE path = ${legal.path}
-      `;
-      return;
-    }
-
-    if (file.kind === 'renamed') {
-      const { path: previousPath } = parseDetailsFromPath(file.previousPath);
-
-      await transaction`
-        UPDATE content.legals
-        SET path = ${legal.path}
-        WHERE path = ${previousPath}
-      `;
-    }
-
-    if (
-      file.kind === 'added' ||
-      file.kind === 'modified' ||
-      file.kind === 'renamed'
-    ) {
-      const lastUpdated = legal.files
-        .filter(
-          (file): file is ModifiedFile | RenamedFile => file.kind !== 'removed',
-        )
-        .sort((a, b) => b.time - a.time)[0];
-
-      const result = await transaction<Legal[]>`
+    const result = await transaction<Legal[]>`
         INSERT INTO content.legals (
           path, name, last_updated, last_commit, last_sync
         )
@@ -69,10 +38,9 @@ export const createProcessMainFile =
         RETURNING id, path, name, last_updated, last_commit, last_sync
       `.then(firstRow);
 
-      if (!result) {
-        throw new Error('Could not insert legal document');
-      }
-
-      return result;
+    if (!result) {
+      throw new Error('Could not insert legal document');
     }
+
+    return result;
   };
