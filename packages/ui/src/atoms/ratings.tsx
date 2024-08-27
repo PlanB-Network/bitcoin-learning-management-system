2716703ab8ac7bdce5529ac2cea834ed756040ef
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-08-27 16:52:55 +0200

    refactor: use Form components on chapter review

--- packages/ui/src/atoms/ratings.tsx
+++ packages/ui/src/atoms/ratings.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import { BsStar, BsStarFill } from 'react-icons/bs';
 
--- packages/ui/tailwind.config-base.ts
+++ packages/ui/tailwind.config-base.ts
@@ -178,6 +178,38 @@ const baseConfig: Config = {
     extend: {
       colors: {
         ring: 'hsl(var(--ring))',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
       },
       borderRadius: {
         lg: `var(--radius)`,
