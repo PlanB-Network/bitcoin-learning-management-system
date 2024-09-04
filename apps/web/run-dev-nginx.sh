48eaa0a5d5fbc203153e888b25891b717c9a9ed2
max-lt <maxime.tissier@gmail.com>
2024-09-04 16:02:03 +0200

    Add additional properties

--- apps/web/run-dev-nginx.sh
+++ apps/web/run-dev-nginx.sh
@@ -8,6 +8,11 @@ scriptpath="$( cd "$(dirname "$0")" ; pwd -P )"
 docker run --rm \
   -p 8181:80 \
   --name nginx-dev \
+  --network cf-tunnel-network \
   -v ${scriptpath}/dist:/usr/share/nginx/html:ro \
   -v ${scriptpath}/docker/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
   nginx:alpine
+
+# docker network create cf-tunnel-network
+# docker network connect cf-tunnel-network nginx-dev
+# docker run --rm --name cf-tunnel-dev --network cf-tunnel-network cloudflare/cloudflared:latest tunnel --no-autoupdate run --token $CF_TUNNEL_TOKEN
