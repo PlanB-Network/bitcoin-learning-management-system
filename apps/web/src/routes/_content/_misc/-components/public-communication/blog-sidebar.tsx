3fba45d395a5e3ce2bd4fbdc0b03df715a53cfcf
Doe <89263198+Flashmaxi@users.noreply.github.com>
2024-09-17 14:55:43 +0200

    fix(public communication): blog sidebar. Rendering articles by category.

--- apps/web/src/routes/_content/_misc/-components/public-communication/blog-sidebar.tsx
+++ apps/web/src/routes/_content/_misc/-components/public-communication/blog-sidebar.tsx
@@ -12,9 +12,13 @@ import { AppContext } from '#src/providers/context.js';
 
 interface BlogSidebarProps {
   currentBlogId: number;
+  currentCategory: string;
 }
 
-const BlogSidebar: React.FC<BlogSidebarProps> = ({ currentBlogId }) => {
+const BlogSidebar: React.FC<BlogSidebarProps> = ({
+  currentBlogId,
+  currentCategory,
+}) => {
   const isScreenMd = useGreater('md');
   const { t } = useTranslation();
   const { blogs } = useContext(AppContext);
@@ -22,6 +26,10 @@ const BlogSidebar: React.FC<BlogSidebarProps> = ({ currentBlogId }) => {
   if (!blogs) {
     return null;
   }
+
+  const filteredBlogs = blogs.filter(
+    (blog) => blog.id !== currentBlogId && blog.category === currentCategory,
+  );
 
   return (
     <div className="mx-auto bg-newGray-6 p-2.5 rounded-2xl">
@@ -34,33 +42,27 @@ const BlogSidebar: React.FC<BlogSidebarProps> = ({ currentBlogId }) => {
       </div>
 
       <ul className="text-black list-none">
-        {blogs.map((blog, index) => {
-          if (blog.id === currentBlogId) {
-            return null;
-          }
-
-          return (
-            <li key={index} className="flex flex-row items-center">
-              <Link
-                to={`/public-communication/blogs-and-news/${blog.category}/${blog.name}`}
-                className="flex items-center justify-between border-t-2 py-3 border-t-gray-600 text-start lg:px-2.5 w.full max-w-[354px]"
-              >
-                <div className="flex flex-row items-center max-w-[280px] lg:max-w-[380px]">
-                  <p className="text-sm text-gray-600 min-w-[73px]">
-                    {formatMonthYear(new Date(blog.lastUpdated))}
-                  </p>
-                  <IoIosArrowForward
-                    size={isScreenMd ? 24 : 16}
-                    className="text-black mx-1.5"
-                  />
-                  <p className="text-sm font-medium truncate min-w-[183px]">
-                    {blog.title}
-                  </p>
-                </div>
-              </Link>
-            </li>
-          );
-        })}
+        {filteredBlogs.map((blog) => (
+          <li key={blog.id} className="flex flex-row items-center">
+            <Link
+              to={`/public-communication/blogs-and-news/${blog.category}/${blog.name}`}
+              className="flex items-center justify-between border-t-2 py-3 border-t-gray-600 text-start lg:px-2.5 w-full max-w-[354px]"
+            >
+              <div className="flex flex-row items-center max-w-[280px] lg:max-w-[380px]">
+                <p className="text-sm text-gray-600 min-w-[73px]">
+                  {formatMonthYear(new Date(blog.lastUpdated))}
+                </p>
+                <IoIosArrowForward
+                  size={isScreenMd ? 24 : 16}
+                  className="text-black mx-1.5"
+                />
+                <p className="text-sm font-medium truncate min-w-[183px] max-w-56">
+                  {blog.title}
+                </p>
+              </div>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
