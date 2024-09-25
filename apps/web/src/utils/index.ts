9c9eafacf2c55ed01542c4535c838d5a0feb5adb
max-lt <maxime.tissier@gmail.com>
2024-09-25 14:34:12 +0200

    Add custom cdn url setter

--- apps/web/src/utils/index.ts
+++ apps/web/src/utils/index.ts
@@ -1,7 +1,15 @@
 export { trpc } from './trpc.ts';
 
+let customCdnUrl = '';
+
+Object.defineProperty(window, 'setCustomCdnUrl', {
+  value: (url: string) => (customCdnUrl = url),
+});
+
 export const computeAssetCdnUrl = (commitHash: string, path: string) => {
-  return `/cdn/${commitHash}/${path}`;
+  return customCdnUrl
+    ? `${customCdnUrl}/${commitHash}/${path}`
+    : `/cdn/${commitHash}/${path}`;
 };
 
 export const compose = (...args: string[]) => args.join(' ');
