17fab91f663a007674db98f64f530f029dd243b0
max-lt <maxime.tissier@gmail.com>
2024-10-17 18:13:37 +0200

    Fix types

--- packages/opentimestamps/src/verify.ts
+++ packages/opentimestamps/src/verify.ts
@@ -7,13 +7,15 @@ interface VerifyOptions {
 type VerifyReturn = { timestamp: number; height: number } | null;
 
 export const createVerify = (options?: VerifyOptions) => {
-  return (otsFile: Buffer, targetHash: string): VerifyReturn => {
+  return async (otsFile: Buffer, targetHash: string): Promise<VerifyReturn> => {
     const detached = ots.DetachedTimestampFile.deserialize(otsFile);
 
     const op = new ots.Ops.OpSHA256();
     const hash = Buffer.from(targetHash, 'hex');
     const original = ots.DetachedTimestampFile.fromHash(op, hash);
 
-    return ots.verify(detached, original, options).bitcoin ?? null;
+    const res = await ots.verify(detached, original, options);
+
+    return res.bitcoin ?? null;
   };
 };
