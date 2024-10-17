6756ef1ab6dca112b5f0a12005089f6bb3069680
max-lt <maxime.tissier@gmail.com>
2024-10-17 10:53:20 +0200

    Add cron service

--- apps/api/src/dependencies.ts
+++ apps/api/src/dependencies.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from 'eventemitter3';
 
+import { type CronService, createCronService } from '@blms/crons';
 import { createPostgresClient } from '@blms/database';
 import type { PostgresClient } from '@blms/database';
 import { RedisClient } from '@blms/redis';
@@ -14,17 +15,15 @@ export interface Dependencies {
   postgres: PostgresClient;
   events: EventEmitter<ApiEvents>;
   config: EnvConfig;
+  crons: CronService;
 }
 
 export const startDependencies = async () => {
+  const crons = createCronService();
   const postgres = createPostgresClient(config.postgres);
-
   const s3 = createS3Service(config.s3);
-
   const redis = new RedisClient(config.redis);
-
   const events = new EventEmitter<ApiEvents>();
-
   await postgres.connect();
 
   const dependencies: Dependencies = {
@@ -33,6 +32,7 @@ export const startDependencies = async () => {
     postgres,
     events,
     config,
+    crons,
   };
 
   const stopDependencies = async () => {
