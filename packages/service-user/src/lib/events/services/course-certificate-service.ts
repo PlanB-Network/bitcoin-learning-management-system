3b1de29ad99c541b9678ce52b903dcb649596f3e
max-lt <maxime.tissier@gmail.com>
2024-10-18 14:03:47 +0200

    Update templates

--- packages/service-user/src/lib/events/services/course-certificate-service.ts
+++ packages/service-user/src/lib/events/services/course-certificate-service.ts
@@ -62,17 +62,7 @@ const formatDate = (date: Date) => {
   const month = date.toLocaleString('default', { month: 'long' });
   const year = date.getFullYear();
 
-  // Determine the correct ordinal suffix for the day
-  const suffix =
-    day % 10 === 1 && day !== 11
-      ? 'st'
-      : day % 10 === 2 && day !== 12
-        ? 'nd'
-        : day % 10 === 3 && day !== 13
-          ? 'rd'
-          : 'th';
-
-  return `${day}${suffix} ${month}, ${year}`;
+  return `${month} ${day}, ${year}`;
 };
 
 interface ExamAttemptWithUser {
