c1f6aff013c082411925c88ddd91587e6eedde5c
max-lt <maxime.tissier@gmail.com>
2025-02-13 15:34:19 +0100

    Fix buffer type

--- packages/service-user/src/lib/courses/services/course-certificate-service.ts
+++ packages/service-user/src/lib/courses/services/course-certificate-service.ts
@@ -217,10 +217,12 @@ export const createExamTimestampService = async (ctx: Dependencies) => {
 
     const { signature, ots, hash } = await timestamp({ text });
 
+    const otfBuffer = new Uint8Array(ots.buffer);
+
     await ctx.postgres.exec(
       sql<UserExamTimestamp[]>`
         INSERT INTO users.exam_timestamps (exam_attempt_id, txt, sig, hash, ots)
-        VALUES (${examAttemptId}, ${text}, ${signature}, ${hash}, ${ots})
+        VALUES (${examAttemptId}, ${text}, ${signature}, ${hash}, ${otfBuffer})
         RETURNING *;
       `,
     );
