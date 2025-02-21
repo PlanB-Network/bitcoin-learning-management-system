7721c1bd6c980e66ec0dd4091a2e118ff286a931
max-lt <maxime.tissier@gmail.com>
2025-02-21 12:37:45 +0100

    Split constants

---
+++ packages/constants/src/auth.ts
@@ -0,0 +1,13 @@
+export enum TokenType {
+  ValidateEmail = 'validate_email',
+  ResetPassword = 'reset_password',
+  Login = 'login',
+}
+
+export enum UserRole {
+  Student = 'student',
+  Professor = 'professor',
+  Community = 'community',
+  Admin = 'admin',
+  Superadmin = 'superadmin',
+}
---
+++ packages/constants/src/courses.ts
@@ -0,0 +1,25 @@
+export enum CourseFormat {
+  Online = 'online',
+  InPerson = 'inperson',
+  Hybrid = 'hybrid',
+}
+
+export enum CoursePaymentFormat {
+  Online = 'online',
+  InPerson = 'inperson',
+}
+
+export enum CoursePaymentMethod {
+  Sbp = 'sbp',
+  Stripe = 'stripe',
+  Free = 'free',
+}
+
+export enum CourseLevel {
+  Beginner = 'beginner',
+  Intermediate = 'intermediate',
+  Advanced = 'advanced',
+  Expert = 'expert',
+  Wizard = 'wizard',
+  Developer = 'developer',
+}
