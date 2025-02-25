73b71011b2c77cec6451dce668e868f3d17bd5ff
max-lt <maxime.tissier@gmail.com>
2025-02-25 09:36:53 +0100

    Fix search query

--- packages/service-content/src/lib/search.ts
+++ packages/service-content/src/lib/search.ts
@@ -222,8 +222,8 @@ const getLectureReplaysQuery = () => sql<Searchable<Language>[]>`
     description as body,
     CONCAT('/', 'en', '/resources/lectures/', id) as link
   FROM content.events
-    WHERE e.type = 'lecture'
-    AND e.end_date < NOW() - INTERVAL '30 days'
+    WHERE events.type = 'lecture'
+    AND events.end_date < NOW() - INTERVAL '30 days'
 `;
 
 const createInitIndexes = (client: TypesenseClient) => () => {
