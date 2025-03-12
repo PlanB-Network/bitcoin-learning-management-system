0bdf5fa37e4d5326467fed46ab2d6b4c9848c556
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-03-12 14:57:32 +0100

    fix: change in tutorial subcategories

--- apps/web/src/services/utils.tsx
+++ apps/web/src/services/utils.tsx
@@ -70,7 +70,7 @@ export const TUTORIALS_CATEGORIES = [
   {
     name: 'contribution',
     image: contributionSvg,
-    subcategories: ['resources', 'content', 'others'],
+    subcategories: ['resource', 'content', 'others'],
     route: '/tutorials/contribution',
   },
 ] as const;
