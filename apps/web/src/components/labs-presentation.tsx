6a52a96d38d30636191d46ec08cbb9772c5028d4
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-03-18 11:03:41 +0100

    feat: add planb labs presentation in course homepage

--- apps/web/src/components/labs-presentation.tsx
+++ apps/web/src/components/labs-presentation.tsx
@@ -6,15 +6,15 @@ import { Button, cn } from '@blms/ui';
 import { useGreater } from '#src/hooks/use-greater.js';
 
 import { useTranslation } from 'react-i18next';
-import BCertImage from '../assets/about/bcert-presentation.webp?no-inline';
+import LabsImage from '../assets/courses/labs-presentation.webp?no-inline';
 
-interface BCertPresentationProps {
+interface LabsPresentationProps {
   marginClasses?: string;
 }
 
-export const BCertPresentation = ({
+export const LabsPresentation = ({
   marginClasses = 'mt-20',
-}: BCertPresentationProps) => {
+}: LabsPresentationProps) => {
   const { t } = useTranslation();
 
   const isScreenMd = useGreater('md');
@@ -26,24 +26,24 @@ export const BCertPresentation = ({
         marginClasses,
       )}
     >
-      <img src={BCertImage} alt="B-CERT" className="w-1/2 max-lg:w-full" />
+      <img src={LabsImage} alt="B-CERT" className="w-full lg:hidden" />
       <div className="flex flex-col w-1/2 max-lg:w-full">
         <h3 className="max-md:mobile-h3 md:desktop-h4 mb-2.5">
-          {t('bCert.knowledgeableBitcoin')}
+          {t('labs.presentation.title')}
         </h3>
         <span className="max-md:mobile-h3 md:text-2xl font-medium leading-tight tracking-[0.25px] text-darkOrange-5 mb-2.5 lg:mb-10">
-          {t('bCert.challengeYourself')}
+          {t('labs.presentation.subtitle')}
         </span>
         <p className="md:desktop-h8 mobile-body1">
-          {t('bCert.bCertDescription')}
+          {t('labs.presentation.content')}
         </p>
-        <Link to="/b-cert" className="max-lg:w-full mt-11">
+        <Link to="/plan-b-labs" className="max-lg:w-full mt-11">
           <Button
             variant="primary"
             size={isScreenMd ? 'l' : 'm'}
             className="max-lg:w-full"
           >
-            {t('bCert.checkout')}
+            {t('labs.presentation.link')}
             <FaArrowRightLong
               className={cn(
                 'opacity-0 max-w-0 inline-flex whitespace-nowrap transition-[max-width_opacity] overflow-hidden ease-in-out duration-150 group-hover:max-w-96 group-hover:opacity-100',
@@ -53,6 +53,7 @@ export const BCertPresentation = ({
           </Button>
         </Link>
       </div>
+      <img src={LabsImage} alt="B-CERT" className="w-1/3 max-lg:hidden" />
     </section>
   );
 };
--- apps/web/src/routes/$lang/_content/courses/index.tsx
+++ apps/web/src/routes/$lang/_content/courses/index.tsx
@@ -8,6 +8,7 @@ import { PageLayout } from '#src/components/page-layout.js';
 
 import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import { LabsPresentation } from '#src/components/labs-presentation.tsx';
 import { AppContext } from '#src/providers/context.tsx';
 import { CourseSelector } from './-components/course-selector.tsx';
 import { CoursesGallery } from './-components/courses-gallery.tsx';
@@ -56,6 +57,9 @@ function CoursesExplorer() {
         {filteredCourses && <CoursesGallery courses={filteredCourses} />}
       </div>
       <div className="border-t border-newGray-1 max-w-[300px] md:max-w-[730px] xl:max-w-[1115px] w-full mx-auto" />
+      <div className="bg-[linear-gradient(180deg,_#000_0%,_#666666_50.5%,_#000_99.5%)] w-full py-5 lg:py-[60px]">
+        <LabsPresentation marginClasses="mt-0 !border-0 !shadow-none text-center lg:text-start" />
+      </div>
       <div className="bg-[linear-gradient(180deg,_#000_0%,_#853000_50.5%,_#000_99.5%)] w-full py-5 lg:py-[60px]">
         <BCertPresentation marginClasses="mt-0 !border-0 !shadow-none text-center lg:text-start" />
       </div>
