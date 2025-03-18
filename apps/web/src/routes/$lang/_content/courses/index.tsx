6f98383343d59924f5a499567b97961baa91e8d3
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-03-18 11:53:23 +0100

    fix: course homepage margin

--- apps/web/src/routes/$lang/_content/courses/index.tsx
+++ apps/web/src/routes/$lang/_content/courses/index.tsx
@@ -57,11 +57,13 @@ function CoursesExplorer() {
         {filteredCourses && <CoursesGallery courses={filteredCourses} />}
       </div>
       <div className="border-t border-newGray-1 max-w-[300px] md:max-w-[730px] xl:max-w-[1115px] w-full mx-auto" />
-      <div className="bg-[linear-gradient(180deg,_#000_0%,_#666666_50.5%,_#000_99.5%)] w-full py-5 lg:py-[60px]">
-        <LabsPresentation marginClasses="mt-0 !border-0 !shadow-none text-center lg:text-start" />
-      </div>
-      <div className="bg-[linear-gradient(180deg,_#000_0%,_#853000_50.5%,_#000_99.5%)] w-full py-5 lg:py-[60px]">
-        <BCertPresentation marginClasses="mt-0 !border-0 !shadow-none text-center lg:text-start" />
+      <div className="py-5 lg:py-[60px]">
+        <div className="bg-[linear-gradient(180deg,_#000_0%,_#666666_50.5%,_#000_99.5%)] w-full">
+          <LabsPresentation marginClasses="mt-0 !border-0 !shadow-none text-center lg:text-start" />
+        </div>
+        <div className="bg-[linear-gradient(180deg,_#000_0%,_#853000_50.5%,_#000_99.5%)] w-full ">
+          <BCertPresentation marginClasses="mt-0 !border-0 !shadow-none text-center lg:text-start" />
+        </div>
       </div>
       <div className="border-t border-newGray-1 max-w-[300px] md:max-w-[730px] xl:max-w-[1115px] w-full mx-auto" />
 
