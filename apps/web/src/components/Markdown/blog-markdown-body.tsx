b519ac435058dfa23f22d1265f3d53271c8b90cc
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-04-01 14:44:45 +0200

    feat: handle course cards when no description only

--- apps/web/src/components/Markdown/blog-markdown-body.tsx
+++ apps/web/src/components/Markdown/blog-markdown-body.tsx
@@ -144,13 +144,15 @@ const BlogMarkdownBody = ({
             );
           }
 
-          const course = getCourse(href, courses ?? []);
-          if (course) {
-            return (
-              <div className="w-full max-w-[500px] md:max-w-[340px] max-md:mx-auto py-2 md:py-1 md:mx-2 md:inline-block md:overflow-hidden">
-                <CourseCard course={course} mode="light" />
-              </div>
-            );
+          if (children === href) {
+            const course = getCourse(href, courses ?? []);
+            if (course) {
+              return (
+                <div className="w-full max-w-[500px] md:max-w-[340px] max-md:mx-auto py-2 md:py-1 md:mx-2 md:inline-block md:overflow-hidden">
+                  <CourseCard course={course} mode="light" />
+                </div>
+              );
+            }
           }
 
           return (
