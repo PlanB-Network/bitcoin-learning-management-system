634448acde76ba3a124230307012e20e2faa6c60
Kevin Mulier <kevin.mulier@outlook.fr>
2025-04-29 19:05:55 +0200

    feat: implement course information retrieval and email notification on payment validation

--- packages/service-user/src/lib/courses/services/update-payment.ts
+++ packages/service-user/src/lib/courses/services/update-payment.ts
@@ -1,4 +1,8 @@
+import { firstRow } from '@blms/database';
 import type { Dependencies } from '../../../dependencies.js';
+import { getUserByIdQuery } from '../../account/queries/get-user.js';
+import { createSendEmail } from '../../account/services/email.js';
+import { getCourseInfo, getCourseLocalized } from '../queries/get-course.js';
 import { updateCourseCoupon } from '../queries/update-course-coupon.js';
 import {
   updateCoursePaymentQuery,
@@ -10,7 +14,10 @@ type Options = { id: string } & (
   | { isPaid: false; isExpired: true }
 );
 
-export const createUpdateCoursePayment = ({ postgres }: Dependencies) => {
+export const createUpdateCoursePayment = ({
+  postgres,
+  config,
+}: Dependencies) => {
   return async (options: Options) => {
     const coursePayment = await postgres.exec(
       updateCoursePaymentQuery(options),
@@ -18,6 +25,46 @@ export const createUpdateCoursePayment = ({ postgres }: Dependencies) => {
 
     if (options.isPaid) {
       await postgres.exec(updateCourseCoupon({ paymentId: options.id }));
+    }
+
+    // Send email to user if course payment is validated and course is teacher-led
+    if (options.isPaid && coursePayment && coursePayment.length === 1) {
+      const courseId = coursePayment[0].courseId;
+
+      const courseInfo = await postgres
+        .exec(getCourseInfo(courseId))
+        .then(firstRow);
+
+      const userInfo = await postgres
+        .exec(getUserByIdQuery(coursePayment[0].uid))
+        .then(firstRow);
+      const userEmail = userInfo?.email;
+
+      if (
+        courseInfo &&
+        courseInfo.teachingFormat === 'professor_led' &&
+        courseInfo.isPlanbSchool &&
+        userEmail
+      ) {
+        const courseLocalized = await postgres
+          .exec(getCourseLocalized('en', courseId))
+          .then(firstRow);
+
+        if (courseLocalized) {
+          const sendEmail = createSendEmail({ config });
+
+          const courseName = courseLocalized.name;
+
+          await sendEmail({
+            email: userEmail,
+            subject: `Welcome to ${courseName}`,
+            template: 'd-fe44ab001b384d40b83090c288f5d3fe',
+            data: {
+              courseName: courseName,
+            },
+          });
+        }
+      }
     }
 
     return coursePayment && coursePayment.length === 1
