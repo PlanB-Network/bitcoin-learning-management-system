8c9c95183dc27f002335b8a0023ac71772f7697b
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-05-07 16:25:10 +0200

    fix: storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,23 +1,31 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@chromatic-com/storybook',
     '@storybook/addon-links',
+    '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+  viteFinal: async (config) => {
+    if (config.optimizeDeps) {
+      config.optimizeDeps.exclude = [
+        ...(config.optimizeDeps.exclude || []),
+        '@storybook/addon-docs',
+        '@storybook/blocks',
+        '@storybook/components',
+        '@storybook/theming',
+        '@storybook/global',
+      ];
+    }
+    return config;
+  },
   docs: {
     autodocs: true,
-  },
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
   },
 };
 
