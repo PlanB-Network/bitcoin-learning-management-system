8dcd4986de6d05629f755e8c51bef5da536bba08
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-06-06 17:27:42 +0200

    fix: import of image

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import { App } from './app.tsx';
 import { AppProvider } from './providers/app.tsx';
 import './utils/i18n';
+import OrangePill from '#src/assets/icons/footer_pill.webp?no-inline';
 
 import '../../../packages/ui/src/styles/global.css';
 
@@ -17,10 +18,10 @@ root.render(
       fallback={
         <div className="flex justify-center items-center h-full w-full">
           <img
+            src={OrangePill}
             className="w-[200px]"
             loading="lazy"
             alt="Orange pill"
-            src="/src/assets/icons/footer_pill.webp?no-inline"
           />
         </div>
       }
