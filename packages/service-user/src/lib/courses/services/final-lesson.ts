e1767c390362794b30be00f80faf7e716a70915e
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-06-19 11:37:35 +0200

    feat(bizschool): withdraws: update email destination to asi0

--- packages/service-user/src/lib/courses/services/final-lesson.ts
+++ packages/service-user/src/lib/courses/services/final-lesson.ts
@@ -34,10 +34,10 @@ export const createWithdrawUserFromCourseFinalLesson = (
     const subject = `${process.env.PLANB_ENVIRONMENT !== 'mainnet' ? '[TEST] - ' : ''} User ${username} withdrawn from final lesson.`;
 
     await sendEmail({
-      email: 'trigger@planb.network',
+      email: 'asi0@planb.network',
       subject: subject,
       template: 'd-10e6dac708224b17bb8b3c2d5336de93',
-      data: { username: username },
+      data: { username: username, subject: subject },
     });
   };
 };
