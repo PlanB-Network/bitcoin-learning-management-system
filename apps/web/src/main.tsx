dc949008d16a6a509e19a732f247081edcf14950
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-08-05 09:14:59 +0200

    fix: disable service worker app update

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,8 +7,6 @@ import './utils/i18n';
 import OrangePill from '#src/assets/icons/footer_pill.webp?no-inline';
 
 import '../../../packages/ui/src/styles/global.css';
-import { customToast } from '@blms/ui';
-import { t } from 'i18next';
 
 const root = ReactDOM.createRoot(
   document.querySelector('#root') as HTMLElement,
@@ -34,28 +32,28 @@ root.render(
 );
 
 // The event listener that is fired when the service worker updates
-navigator.serviceWorker.addEventListener('controllerchange', (e) => {
-  customToast(t('notifications.newVersionAvailable'), {
-    color: 'primary',
-    time: 10000,
-    closeButton: false,
-    onClick: () => {
-      window.location.reload();
-    },
-  });
-});
+// navigator.serviceWorker.addEventListener('controllerchange', (e) => {
+//   customToast(t('notifications.newVersionAvailable'), {
+//     color: 'primary',
+//     time: 10000,
+//     closeButton: false,
+//     onClick: () => {
+//       window.location.reload();
+//     },
+//   });
+// });
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        setInterval(() => {
-          registration.update();
-        }, 10 * 1000);
-      })
-      .catch((error) =>
-        console.error('Service Worker registration failed:', error),
-      );
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker
+//       .register('/service-worker.js')
+//       .then((registration) => {
+//         setInterval(() => {
+//           registration.update();
+//         }, 10 * 1000);
+//       })
+//       .catch((error) =>
+//         console.error('Service Worker registration failed:', error),
+//       );
+//   });
+// }
