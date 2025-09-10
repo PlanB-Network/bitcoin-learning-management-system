4e58de8fe8e38765c1d81f204b2374b677425206
max-lt <maxime.tissier@gmail.com>
2025-09-10 11:51:31 +0200

    Pear layout

--- apps/contribute/src/components/Header/header.tsx
+++ apps/contribute/src/components/Header/header.tsx
@@ -32,7 +32,7 @@ export const Header = ({
   return (
     <header
       className={cn(
-        'sticky left-0 top-0 z-40 flex w-full flex-row justify-between py-[11px] px-4 lg:min-h-[96px] lg:px-12 lg:py-3',
+        'pt-if-pear sticky left-0 top-0 z-40 flex w-full flex-row justify-between py-[11px] px-4 lg:min-h-[96px] lg:px-12 lg:py-3',
         variant === 'light' ? 'bg-darkOrange-5' : 'bg-headerDark',
       )}
     >
--- apps/web/src/components/main-layout.tsx
+++ apps/web/src/components/main-layout.tsx
@@ -168,7 +168,7 @@ export const MainLayout = ({
           )}
           style={{ willChange: 'margin-left' }}
         >
-          <main className="flex grow flex-col bg-white min-h-[calc(100vh-64px)] lg:min-h-[calc(100vh-72px)] w-full relative">
+          <main className="mt-if-pear flex grow flex-col bg-white min-h-[calc(100vh-64px)] lg:min-h-[calc(100vh-72px)] w-full relative">
             {children}
           </main>
           {showFooter && <Footer />}
@@ -202,7 +202,7 @@ const SideBar = ({ isSidebarOpen }: { isSidebarOpen: boolean }) => {
   return (
     <nav
       className={cn(
-        'fixed flex flex-col top-18 px-4 h-[calc(100vh-72px)] overflow-y-auto max-lg:hidden no-scrollbar transition-all ease-in-out gap-3',
+        'pt-if-pear fixed flex flex-col top-18 px-4 h-[calc(100vh-72px)] overflow-y-auto max-lg:hidden no-scrollbar transition-all ease-in-out gap-3',
         isSidebarOpen ? openWidth : closedWidth,
       )}
       // help the browser optimize the animation
