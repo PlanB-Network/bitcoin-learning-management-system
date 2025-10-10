c3b358bf2336c2ac6701ca12d40b79b195aeeb0a
kevinmulier <kevin.mulier.dev@gmail.com>
2025-10-10 16:52:28 +0200

    feat(search): improve search result layout and styling

--- apps/web/src/routes/$lang/_content/search/-components/search-result.tsx
+++ apps/web/src/routes/$lang/_content/search/-components/search-result.tsx
@@ -2,6 +2,7 @@ import type { Searchable, SearchResultItem } from '@blms/types';
 import { TextTag } from '@blms/ui';
 import { default as DOMPurify } from 'dompurify';
 import { useTranslation } from 'react-i18next';
+import { TbChevronRight } from 'react-icons/tb';
 import { useSmaller } from '#src/hooks/use-smaller.ts';
 
 interface SearchResultProps {
@@ -15,38 +16,46 @@ export const SearchResult = ({ item }: SearchResultProps) => {
 
   return (
     <a
-      className="flex flex-col gap-1 md:gap-2 rounded-2xl px-0.5 py-1 md:px-4 md:py-3 hover:bg-neutral-50 border border-transparent focus:border-orange-500 focus:outline-hidden search-results"
+      className="flex gap-2 rounded-2xl px-0.5 py-1 md:px-4 md:py-3 hover:bg-neutral-50 border border-transparent focus:border-orange-500 focus:outline-hidden search-results w-full justify-between items-center"
       href={`${item.document.link}#:~:text=${item.highlight.body?.matched_tokens?.[0] ?? item.document.title}`}
       target="_blank"
       rel="noreferrer"
     >
-      <div className="flex gap-2.5 items-center">
-        <TextTag mode="light" variant="grey" size={isMobile ? 'small' : 'base'}>
-          {t(`search.${item.document.type}`)}
-        </TextTag>
+      <div className="flex flex-col gap-1 md:gap-2">
+        <div className="flex gap-2.5 items-center">
+          <TextTag
+            mode="light"
+            variant="grey"
+            size={isMobile ? 'small' : 'base'}
+            className="shrink-0"
+          >
+            {t(`search.${item.document.type}`)}
+          </TextTag>
 
-        {item.highlight.title ? (
+          {item.highlight.title ? (
+            <div
+              className="body-base md:label"
+              // biome-ignore lint/security/noDangerouslySetInnerHtml: html is sanitized
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(item.highlight.title?.snippet ?? ''),
+              }}
+            />
+          ) : (
+            <div className="body-base md:label">{item.document.title}</div>
+          )}
+        </div>
+
+        {item.highlight.body && (
           <div
-            className="body-base md:label"
+            className="text-neutral-600 body-extra-small md:body-small"
             // biome-ignore lint/security/noDangerouslySetInnerHtml: html is sanitized
             dangerouslySetInnerHTML={{
-              __html: DOMPurify.sanitize(item.highlight.title?.snippet ?? ''),
+              __html: DOMPurify.sanitize(item.highlight.body?.snippet ?? ''),
             }}
           />
-        ) : (
-          <div className="body-base md:label">{item.document.title}</div>
         )}
       </div>
-
-      {item.highlight.body && (
-        <div
-          className="text-neutral-600 body-extra-small md:body-small"
-          // biome-ignore lint/security/noDangerouslySetInnerHtml: html is sanitized
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(item.highlight.body?.snippet ?? ''),
-          }}
-        />
-      )}
+      <TbChevronRight size={16} className="shrink-0" />
     </a>
   );
 };
