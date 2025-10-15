2d1679bc81583736d7632dac28ee0d296645c30e
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-10-15 10:53:22 +0200

    feat(network): start hubs page

---
+++ apps/network/src/components/media-card2.tsx
@@ -0,0 +1,95 @@
+import { cn } from '@blms/ui';
+import { titleCss } from '#src/utils/css.tsx';
+import { NetworkButton } from './network-button.tsx';
+import PageBlock from './page-block.tsx';
+
+type MediaCard2Props = {
+  title: string;
+  subtext: string;
+  imageUrl: string;
+  className?: string;
+  titleClassName?: string;
+  subtitleClassName?: string;
+  imageClassName?: string;
+  alt?: string;
+  orientation?: 'left' | 'right';
+  subtitleUnderImage?: boolean;
+  NoBackground?: boolean;
+};
+
+export default function MediaCard2({
+  title,
+  subtext,
+  imageUrl,
+  className,
+  titleClassName = 'max-w-[800px]',
+  subtitleClassName = 'max-w-[700px]',
+  imageClassName = '',
+  alt = '',
+  orientation = 'left',
+  NoBackground = false,
+}: MediaCard2Props) {
+  const isLeft = orientation === 'left';
+
+  return (
+    <PageBlock
+      className={cn(
+        'shadow-lg transition-transform hover:-translate-y-0.5 hover:shadow-xl h-full border-orange-500 bg-gradient-network-bt-transparent',
+        isLeft
+          ? 'rounded-b-2xl border-b-[1px] border-l-[1px]'
+          : 'rounded-b-2xl border-b-[1px] border-r-[1px]',
+        className,
+      )}
+    >
+      <fieldset
+        aria-labelledby="media-card-title"
+        className={[
+          'w-full flex flex-col items-center ',
+          isLeft ? 'lg:flex-row' : 'lg:flex-row-reverse ',
+          NoBackground ? '' : 'max-lg:bg-network-cards',
+        ].join(' ')}
+      >
+        <div
+          className={cn(
+            'w-[50%] h-full z-10 p-5 lg:mt-4 flex flex-col gap-12 lg:pb-10',
+            !isLeft ? ' lg:items-end lg:self-end' : '',
+          )}
+        >
+          <h3
+            className={cn(
+              titleCss,
+              titleClassName,
+              'w-full',
+              isLeft ? 'lg:text-start' : 'lg:text-end',
+            )}
+          >
+            {title}
+          </h3>
+
+          <p
+            className={cn(
+              'text-center text-base lg:text-xl max-lg:font-normal max-lg:mt-8 title-bas text-gray-300 whitespace-pre-wrap',
+              subtitleClassName ?? '',
+              isLeft ? 'lg:text-start' : 'lg:text-end',
+            )}
+          >
+            {subtext}
+          </p>
+          <NetworkButton variant={'secondary'}>TODO</NetworkButton>
+        </div>
+        <div
+          className={cn(
+            'w-[50%] flex flex-row',
+            isLeft ? 'justify-end' : 'justify-start',
+          )}
+        >
+          <img
+            src={imageUrl}
+            alt={alt}
+            className={['object-cover ', imageClassName].join(' ')}
+          />
+        </div>
+      </fieldset>
+    </PageBlock>
+  );
+}
--- apps/network/src/routes/hubs.tsx
+++ apps/network/src/routes/hubs.tsx
@@ -1,8 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Trans, useTranslation } from 'react-i18next';
+import coworking1Image from '#src/assets/hubs/coworking-1.png';
+import coworking2Image from '#src/assets/hubs/coworking-2.png';
+import coworking3Image from '#src/assets/hubs/coworking-3.png';
 import headerImage from '#src/assets/hubs/header.png';
 import headerSmallImage from '#src/assets/hubs/header.png';
+import luganoBgImage from '#src/assets/hubs/lugano-bg.png';
+import luganoLogo from '#src/assets/hubs/lugano-planb-logo.png';
+import locationImage from '#src/assets/icons/location.svg';
+import BlockTitle from '#src/components/block-title.tsx';
 import { Hero } from '#src/components/hero.tsx';
+import MediaCard2 from '#src/components/media-card2.tsx';
+import PageBlock from '#src/components/page-block.tsx';
 import { useSmaller } from '#src/hooks/use-smaller.ts';
 
 export const Route = createFileRoute('/hubs')({
@@ -27,7 +36,86 @@ function RouteComponent() {
         titleClassName="max-w-[65%]"
         subtitleClassName={'max-w-[85%] lg:max-w-[450px]'}
       />
-      <div />
+      <PageBlock className="bg-gradient-network-lr-dark mt-8 p-10">
+        <h1 className="title-extra-large">{t('hubs.co-working.title')}</h1>
+        <p className="mt-8 max-w-[466px]">{t('hubs.co-working.subtitle')}</p>
+        <div className="flex flew-row justify-between gap-8 mt-16">
+          <CoworkingCard
+            img={coworking1Image}
+            name={t('hubs.co-working.hub1Name')}
+            location={t('hubs.co-working.hub1Location')}
+          />
+          <CoworkingCard
+            img={coworking2Image}
+            name={t('hubs.co-working.hub2Name')}
+            location={t('hubs.co-working.hub2Location')}
+          />
+          <CoworkingCard
+            img={coworking3Image}
+            name={t('hubs.co-working.hub3Name')}
+            location={t('hubs.co-working.hub3Location')}
+          />
+        </div>
+      </PageBlock>
+
+      <BlockTitle
+        text={t('hubs.blockTitle1Text')}
+        subtext={t('hubs.blockTitle1Subtext')}
+      />
+
+      <PageBlock className="relative !max-w-[1400px]">
+        <img
+          className="z-0 absolute -ml-12 -mt-10 w-full"
+          src={luganoBgImage}
+          alt="Lugano's lake"
+        />
+        <h2 className="relative z-10 mt-12 title-extra-large text-6xl max-w-[800px]">
+          {t('hubs.lugano.superTitle')}
+        </h2>
+        <p className="relative z-1 title-medium max-w-[600px] mt-8">
+          {t('hubs.lugano.superSubtitle')}
+        </p>
+      </PageBlock>
+
+      <MediaCard2
+        title={t('hubs.lugano.title')}
+        subtext={t('hubs.lugano.subtitle')}
+        imageUrl={luganoLogo}
+        alt=""
+        className="mt-24 z-10 relative"
+        subtitleClassName="!text-gray-100"
+        orientation="left"
+      />
+
+      <MediaCard2
+        title={t('hubs.lugano.title')}
+        subtext={t('hubs.lugano.subtitle')}
+        imageUrl={luganoLogo}
+        alt=""
+        className="mt-24"
+        orientation="right"
+      />
     </>
+  );
+}
+
+function CoworkingCard({
+  img,
+  name,
+  location,
+}: {
+  img: string;
+  name: string;
+  location: string;
+}) {
+  return (
+    <div className="flex flex-col items-center gap-10 max-w-[400px] bg-black pb-6 rounded-4xl">
+      <img src={img} alt="coworking 1" />
+      <span className="display-base">{name}</span>
+      <div className="flex flex-col items-center gap-2">
+        <img src={locationImage} alt="location" className="w-8" />
+        <span className="title-small text-orange-500">{location}</span>
+      </div>
+    </div>
   );
 }
