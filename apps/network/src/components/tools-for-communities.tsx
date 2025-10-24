5b9452c49069980d1c4b63d3248e36d8cbdf4db8
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-10-24 12:23:11 +0200

    feat(network): use image component in tools

--- apps/network/src/components/tools-for-communities.tsx
+++ apps/network/src/components/tools-for-communities.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@blms/ui';
+import { cn, Image } from '@blms/ui';
 import { Link } from '@tanstack/react-router';
 import { Trans, useTranslation } from 'react-i18next';
 import communityImg from '#src/assets/community/community-img.webp';
@@ -64,10 +64,12 @@ function BlockOne() {
     >
       <div className="h-fit w-full mx-2 px-4 flex flex-col border-1 border-orange-500 rounded-[40px] lg:max-w-[550px] xl:max-w-[670px] bg-[#100600] hover:bg-[#3F1700]">
         <div className="mt-5 lg:mt-8 flex flex-row w-full gap-6 pl-5">
-          <img
+          <Image
             className="max-lg:h-16 object-cover "
             src={communityLogo}
             alt=""
+            loading="lazy"
+            breakpoints={{ default: 500 }}
           />
           <div className="flex flex-col gap-4 title-large max-w-[350px]">
             <span>
@@ -83,10 +85,12 @@ function BlockOne() {
         <span className="lg:hidden my-5 body-12px-small text-gray-200 ">
           {t('community.box1Subtitle')}
         </span>
-        <img
+        <Image
           className="lg:mt-5 rounded-4xl rounded-b-3xl"
           src={communityImg}
           alt=""
+          loading="lazy"
+          breakpoints={{ default: 1000 }}
         />
       </div>
     </Link>
@@ -107,10 +111,12 @@ function BlockTwo() {
     >
       <div className="w-full mx-2 px-4 flex flex-col border-1 border-blue-400 rounded-[40px] lg:max-w-[387px] xl:max-w-[507px] bg-blue-950 hover:bg-[#032042]">
         <div className="mt-5 lg:mt-8 flex flex-row w-full gap-6 px-4">
-          <img
+          <Image
             className="max-lg:h-16 object-cover "
             src={languageLogo}
             alt=""
+            loading="lazy"
+            breakpoints={{ default: 500 }}
           />
           <div className="flex flex-col gap-4 title-large max-w-[330px]">
             <span>
@@ -123,7 +129,13 @@ function BlockTwo() {
         <span className="my-5 body-12px-small lg:title-small text-gray-200 lg:max-w-[400px] lg:ml-10 ">
           {t('community.box2Subtitle')}
         </span>
-        <img className="lg:mt-5 rounded-t-4xl" src={languageImg} alt="" />
+        <Image
+          className="lg:mt-5 rounded-t-4xl"
+          src={languageImg}
+          alt=""
+          loading="lazy"
+          breakpoints={{ default: 1000 }}
+        />
       </div>
     </Link>
   );
@@ -139,10 +151,12 @@ function BlockThree() {
     >
       <div className="w-full mx-2 flex flex-col border-1 border-yellow-4 rounded-[40px] lg:max-w-[450px] xl:max-w-[530px] bg-[#1B1000] hover:bg-[#402704]">
         <div className="px-4 mt-5 lg:mt-8 flex flex-row w-full gap-6">
-          <img
+          <Image
             className="max-lg:h-16 object-cover "
             src={resourceLogo}
             alt=""
+            loading="lazy"
+            breakpoints={{ default: 500 }}
           />
           <div className="flex flex-col gap-4 title-large max-w-[330px]">
             <span>
@@ -152,10 +166,12 @@ function BlockThree() {
             </span>
           </div>
         </div>
-        <img
+        <Image
           className="mt-10 rounded-4xl w-fit pl-8"
           src={resourceImg}
           alt=""
+          loading="lazy"
+          breakpoints={{ default: 1000 }}
         />
       </div>
     </Link>
@@ -174,7 +190,13 @@ function BlockFour() {
     >
       <div className="w-full pt-2 h-fit mx-2 flex flex-col border-1 border-purple-400 rounded-[40px] lg:max-w-[480px] xl:max-w-[595px] bg-purple-950 hover:bg-[#3B0746]">
         <div className="px-4 mt-5 lg:mt-8 flex flex-row w-full gap-6">
-          <img className="max-lg:h-16" src={eventsLogo} alt="" />
+          <Image
+            className="max-lg:h-16"
+            src={eventsLogo}
+            alt=""
+            loading="lazy"
+            breakpoints={{ default: 500 }}
+          />
           <div className="flex flex-col gap-4 title-large">
             <span>
               <Trans i18nKey="community.box4Title">
@@ -189,10 +211,12 @@ function BlockFour() {
         <span className="lg:hidden px-5 my-5 body-12px-small text-gray-200 ">
           {t('community.box4Subtitle')}
         </span>
-        <img
+        <Image
           className="lg:mt-5 rounded-4xl  pl-20 pr-15"
           src={eventsImg}
           alt=""
+          loading="lazy"
+          breakpoints={{ default: 1000 }}
         />
       </div>
     </Link>
@@ -209,7 +233,13 @@ function BlockFive() {
     >
       <div className="w-full mx-2 px-6 flex flex-col border-1 border-green-400 rounded-[40px] lg:max-w-[414px] xl:max-w-[534px] bg-[#001700] hover:bg-[#002600]">
         <div className="mt-5 lg:mt-8 flex flex-row w-full gap-6 px-4">
-          <img className="max-lg:h-16 object-cover " src={safeLogo} alt="" />
+          <Image
+            className="max-lg:h-16 object-cover "
+            src={safeLogo}
+            alt=""
+            loading="lazy"
+            breakpoints={{ default: 500 }}
+          />
           <div className="flex flex-col gap-4 title-large max-w-[330px]">
             <span>
               <Trans i18nKey="community.box5Title">
@@ -220,13 +250,21 @@ function BlockFive() {
         </div>
         <div className="flex flex-row pr-10 pl-7">
           <div className="w-[50%]">
-            <img className="mt-10 w-full" src={safeButtonsImage} alt="" />
+            <Image
+              className="mt-10 w-full"
+              src={safeButtonsImage}
+              alt=""
+              loading="lazy"
+              breakpoints={{ default: 1000 }}
+            />
           </div>
           <div className="w-[50%]">
-            <img
+            <Image
               className="rounded-t-4xl translate-x-4 w-full"
               src={safeImg}
               alt=""
+              loading="lazy"
+              breakpoints={{ default: 1000 }}
             />
           </div>
         </div>
@@ -247,7 +285,13 @@ function BlockSix() {
     >
       <div className="w-full h-fit pb-2 mx-2 pr-6 flex flex-col gap-6 border-1 border-orange-500 rounded-[40px] lg:max-w-[480px] xl:max-w-[600px] bg-[#100600] hover:bg-orange-950">
         <div className="mt-5 lg:mt-8 flex flex-row w-full gap-6 px-5">
-          <img className="max-lg:h-16 object-cover " src={testLogo} alt="" />
+          <Image
+            className="max-lg:h-16 object-cover "
+            src={testLogo}
+            alt=""
+            loading="lazy"
+            breakpoints={{ default: 500 }}
+          />
           <div className="flex flex-col gap-4 title-large max-w-[330px]">
             <span>
               <Trans i18nKey="community.box6Title">
@@ -260,7 +304,13 @@ function BlockSix() {
           <p className="title-small text-gray-200 lg:w-[50%]">
             {t('community.box6Subtitle')}
           </p>
-          <img className="lg:w-[50%] pl-4 pb-3" src={testImg} alt="" />
+          <Image
+            className="lg:w-[50%] pl-4 pb-3"
+            src={testImg}
+            alt=""
+            loading="lazy"
+            breakpoints={{ default: 1000 }}
+          />
         </div>
       </div>
     </Link>
