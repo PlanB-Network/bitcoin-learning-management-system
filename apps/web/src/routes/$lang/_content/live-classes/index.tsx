bbea79f4a42b8cbd407687102cadacebb3d68f9a
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-11-17 15:21:28 +0100

    feat(live-classes): program part

--- apps/web/src/routes/$lang/_content/live-classes/index.tsx
+++ apps/web/src/routes/$lang/_content/live-classes/index.tsx
@@ -1,14 +1,20 @@
 import { TeachingFormat } from '@blms/constants';
-import { Button, Loader, Progress } from '@blms/ui';
+import { getCountryForFlagFromAddress } from '@blms/shared';
+import type { CourseResponse, JoinedCourse } from '@blms/types';
+import { Button, cn, Flag, Image, Loader, Progress } from '@blms/ui';
 import { useQuery } from '@tanstack/react-query';
 import { createFileRoute, Link } from '@tanstack/react-router';
+import { t } from 'i18next';
 import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
-import banner from '#src/assets/courses/live-classes-banner.webp';
+import { TbCalendarEvent, TbChevronRight, TbClock } from 'react-icons/tb';
 import OrangePill from '#src/assets/icons/orange_pill_color.svg';
+import programMainImage from '#src/assets/programs/program-main.webp';
 import { PageLayout } from '#src/components/page-layout.js';
 import { CourseCardBig } from '#src/patterns/course-card-big.tsx';
 import { AppContext } from '#src/providers/context.tsx';
+import { formatShortDateRange } from '#src/utils/date.ts';
+import { resourceImgUrl } from '#src/utils/index.ts';
 import { normalizeString } from '#src/utils/string.ts';
 import { trpc } from '#src/utils/trpc.ts';
 
@@ -157,33 +163,18 @@ function AllCourses() {
         {t('courses.liveClasses.title')}
       </h2>
 
-      <div className="bg-vertical-orange-gradient border-1 border-orange-200 rounded-2xl">
+      <div className="bg-vertical-orange-gradient border border-orange-200 rounded-2xl">
         <div className="max-lg:hidden flex flex-col">
-          <img
-            className="w-full object-cover"
-            src={banner}
-            alt="A professor on stage"
-          />
-
-          <div className="bottom-2 px-6 w-full self-end z-10 -mt-16">
+          <div className=" py-6 px-6 w-full">
             <h2 className="display-base">{t('courses.liveClasses.title')}</h2>
-            <h3 className="label-18px text-neutral-600">
-              {t('courses.liveClasses.secondaryTitle')}
-            </h3>
           </div>
-          <Link to={'/programs'}>
-            <Button className="ml-6 mt-4" size={'xl'}>
-              {t('courses.learnMore')} <span className="ml-6">{' >'}</span>
-            </Button>
-          </Link>
+          <Link to={'/programs'} />
         </div>
 
         <div className="flex flex-wrap gap-4 lg:gap-8 mt-2 lg:mt-4 px-2 lg:px-6 pb-2 lg:pb-6">
-          {planbCourses.map((course) => (
-            <div key={course.id} className="w-full">
-              <CourseCardBig course={course} />
-            </div>
-          ))}
+          <div className="w-full">
+            <ProgramCard course={planbCourses.at(0)!} />
+          </div>
         </div>
       </div>
 
@@ -207,3 +198,125 @@ function AllCourses() {
     </PageLayout>
   );
 }
+
+const ProgramCard = ({ course }: { course: JoinedCourse | CourseResponse }) => {
+  const dateString = formatShortDateRange(course.startDate, course.endDate);
+
+  return (
+    <Link
+      key={course.id}
+      to={'/programs'}
+      className={cn(
+        'flex w-full max-lg:mx-auto max-lg:max-w-[340px] bg-white rounded-2xl',
+      )}
+    >
+      <article
+        className={'flex flex-row w-full border border-neutral-50 rounded-2xl'}
+      >
+        <DesktopCourseThumbnail />
+        <div className="grow min-w-0 w-full">
+          <MobileCourseThumbnail />
+
+          <div className="flex flex-col p-4 h-full w-full">
+            <span className="mt-4 flex flex-col w-full line-clamp-2 title-medium align-top mb-2 lg:mb-0">
+              {t('courses.liveClasses.program.title')}
+            </span>
+            <p className="text-neutral-600 body-small lg:line-clamp-4">
+              {t('courses.liveClasses.program.goal')}
+            </p>
+
+            <div className="flex flex-row my-4 items-center">
+              {course.mainProfessors.map((professor) => (
+                <Image
+                  key={professor.id}
+                  src={resourceImgUrl(professor, 'profile.webp')}
+                  alt={professor.name}
+                  breakpoints={{ default: 100, lg: 150 }}
+                  className={cn(
+                    'size-6 rounded-full z-10 object-cover [overflow-clip-margin:_unset]',
+                  )}
+                />
+              ))}
+
+              <span className="ml-2 max-lg:body-small">
+                {course.mainProfessors.map((professor) => professor.name)}
+              </span>
+            </div>
+
+            <div className="w-full lg:mt-auto flex flex-col flex-wrap lg:flex-row gap-5 lg:gap-2 text-nowrap justify-between overflow-hidden border-t-1 border-neutral-50 pt-5">
+              <div className="grow-3 body-base-bold flex flew-row gap-1 mx-2">
+                {course.format === 'online' || course.format === 'hybrid' ? (
+                  <span>{t('accessType.online')}</span>
+                ) : null}
+                {course.format === 'hybrid' ? (
+                  <span className="text-neutral-100">|</span>
+                ) : null}
+                {course.format === 'inperson' || course.format === 'hybrid' ? (
+                  <div className="flex flex-row gap-2">
+                    <Flag
+                      code={getCountryForFlagFromAddress(
+                        course.addressLine1 || '',
+                      )}
+                      size="s"
+                      className="self-center"
+                      isRound={true}
+                    />
+                    <span>{course.addressLine1}</span>
+                  </div>
+                ) : null}
+              </div>
+              <div className="grow flex flex-row gap-2 items-center mx-2">
+                <TbCalendarEvent className="h-5 w-5 text-brown-400" />
+                <span className="body-base-bold text-brown-800 ">
+                  {dateString}
+                </span>
+              </div>
+              <div className="max-lg:hidden flex flex-row gap-2 items-center mx-2">
+                <TbClock className="h-5 w-5 text-brown-400" />
+                <span className="body-base-bold text-brown-800">
+                  {t('courses.liveClasses.program.semester')}
+                </span>
+              </div>
+
+              <Button
+                variant="primary"
+                className="lg:hidden w-full mt-2"
+                size={'m'}
+              >
+                {t('words.discover')}
+                <span className="ml-2">{'>'}</span>
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="max-lg:hidden self-center mx-4 w-fit">
+          <TbChevronRight className="h-5 w-5 text-neutral-300" />
+        </div>
+      </article>
+    </Link>
+  );
+};
+
+const DesktopCourseThumbnail = () => {
+  return (
+    <img
+      className={
+        'max-lg:hidden w-[230px] xl:w-[338px] rounded-l-2xl object-cover'
+      }
+      src={programMainImage}
+      alt=""
+    />
+  );
+};
+
+const MobileCourseThumbnail = () => {
+  return (
+    <div className="w-full">
+      <img
+        className={'lg:hidden rounded-t-2xl h-[172px] w-full object-cover'}
+        src={programMainImage}
+        alt=""
+      />
+    </div>
+  );
+};
