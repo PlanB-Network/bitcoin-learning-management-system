0d4dc0fd6373f2760d0c4a39d0484a0c550604fc
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-12-17 23:46:45 +0100

    feat: update radio-group ui

--- packages/ui/src/bases/radio-group.tsx
+++ packages/ui/src/bases/radio-group.tsx
@@ -1,6 +1,5 @@
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
 import type * as React from 'react';
-import { TbCircle } from 'react-icons/tb';
 import { cn } from '#src/lib/utils.ts';
 
 function RadioGroup({
@@ -24,20 +23,21 @@ function RadioGroupItem({
     <RadioGroupPrimitive.Item
       data-slot="radio-group-item"
       className={cn(
-        'aspect-square size-4 shrink-0 rounded-full border border-neutral-200 shadow-xs outline-none',
-        'focus-visible:ring-2 focus-visible:ring-blue-300 focus-visible:ring-offset-2 hover:border-orange-400',
-        'disabled:cursor-not-allowed disabled:opacity-50',
-        'data-[state=checked]:bg-orange-500 data-[state=checked]:border-transparent',
+        'aspect-square size-4 shrink-0 rounded-full border-2 border-neutral-500 bg-white outline-none',
+        'focus-visible:ring-4 focus-visible:ring-neutral-200',
+        'hover:border-orange-400',
+        'disabled:cursor-not-allowed disabled:border-neutral-200 disabled:bg-neutral-100',
+        'data-[state=checked]:border-orange-500 data-[state=checked]:border-5',
+        'data-[state=checked]:focus-visible:ring-orange-200',
+        'data-[state=checked]:disabled:border-neutral-300',
         className,
       )}
       {...props}
     >
       <RadioGroupPrimitive.Indicator
         data-slot="radio-group-indicator"
-        className="relative flex items-center justify-center"
-      >
-        <TbCircle className="fill-primary absolute top-1/2 left-1/2 size-3 -translate-x-1/2 -translate-y-1/2" />
-      </RadioGroupPrimitive.Indicator>
+        className="flex items-center justify-center"
+      />
     </RadioGroupPrimitive.Item>
   );
 }
