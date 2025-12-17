c508a0976f8dccd69395c90a9f59de6a338f0f8b
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-12-17 09:26:06 +0100

    fix(ec): update db script

--- packages/database/drizzle/migrations/0151_crazy_matthew_murdock.sql
+++ packages/database/drizzle/migrations/0151_crazy_matthew_murdock.sql
@@ -1,1 +1,4 @@
-ALTER TYPE "public"."educator_content_type" ADD VALUE 'others' BEFORE 'presentation';
+ALTER TABLE "content"."educator_contents" ALTER COLUMN "type" SET DATA TYPE text;--> statement-breakpoint
+DROP TYPE "public"."educator_content_type";--> statement-breakpoint
+CREATE TYPE "public"."educator_content_type" AS ENUM('art','booklet', 'flyer', 'game', 'other', 'presentation', 'sticker', 'workshop');--> statement-breakpoint
+ALTER TABLE "content"."educator_contents" ALTER COLUMN "type" SET DATA TYPE "public"."educator_content_type" USING "type"::"public"."educator_content_type";
