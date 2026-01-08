9fff76c15fbded9c9ab5d545ccfdee1eb36ca4e6
trigger67 <10185784+trigger67@users.noreply.github.com>
2026-01-08 15:26:10 +0100

    chore: do not log errors linked to getUserNotifications

--- apps/api/src/trpc/index.ts
+++ apps/api/src/trpc/index.ts
@@ -58,9 +58,12 @@ export const createContext = (
  */
 const t = initTRPC.context<Context>().create({
   errorFormatter({ ctx, shape, error }) {
-    ctx?.log(
-      `ERROR (${shape.data.path}): ${error.message} ${error.name} ${error.code} `,
-    );
+    if (shape.data.path && !shape.data.path.includes('getUserNotifications')) {
+      ctx?.log(
+        `ERROR (${shape.data.path}): ${error.message} ${error.name} ${error.code} `,
+      );
+    }
+
     return {
       ...shape,
       data: {
