1ffa0d542cd5503d08b0141bda43417841ce3816
kevinmulier <kevin.mulier.dev@gmail.com>
2026-01-20 15:48:02 +0100

    feat: update CourseTitle component to use Link for navigation

--- apps/academy/src/routes/$lang/_course/courses/$courseSlug/-components/course-title.tsx
+++ apps/academy/src/routes/$lang/_course/courses/$courseSlug/-components/course-title.tsx
@@ -1,10 +1,14 @@
 import type { CourseResponse } from '@blms/types';
 import { TextTag } from '@blms/ui';
+import { Link } from '@tanstack/react-router';
 import { addSpaceToCourseIndex } from '#src/utils/courses.ts';
 
 export const CourseTitle = ({ course }: { course: CourseResponse }) => {
   return (
-    <div className="flex items-center gap-2">
+    <Link
+      to={`/courses/${course.id}`}
+      className="flex items-center gap-2 group"
+    >
       <TextTag
         size="small"
         variant="brown"
@@ -12,9 +16,9 @@ export const CourseTitle = ({ course }: { course: CourseResponse }) => {
       >
         {addSpaceToCourseIndex(course.index)}
       </TextTag>
-      <h3 className="subtitle-base text-neutral-900 line-clamp-1">
+      <h3 className="subtitle-base text-neutral-900 line-clamp-1 group-hover:text-orange-500">
         {course.name}
       </h3>
-    </div>
+    </Link>
   );
 };
