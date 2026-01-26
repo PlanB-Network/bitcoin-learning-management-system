5b3cd82328b7dae582e99242c8b9cf1552497c45
trigger67 <10185784+trigger67@users.noreply.github.com>
2026-01-26 14:27:43 +0100

    feat: hard remove for unused coupon codes

--- packages/service-content/src/lib/coupon/delete-coupon-code.ts
+++ packages/service-content/src/lib/coupon/delete-coupon-code.ts
@@ -28,34 +28,47 @@ export const createDeleteCourseCouponCodeCoordinator = ({
 
     console.log('Delete coupon code', code, 'by coordinator with uid', userId);
 
-    return postgres
-      .exec(sql<CouponCode[]>`
-        UPDATE content.coupon_code
-        SET deleted_at = NOW()
-        WHERE code = ${code}
-        AND item_id = ${itemId}
-        RETURNING *;
-      `)
-      .then(firstRow)
-      .then(rejectOnEmpty);
+    return deleteCoupon(postgres, code, itemId);
   };
 };
 
 export const createDeleteCouponCode = ({
   postgres,
 }: Pick<Dependencies, 'postgres'>) => {
-  return (code: string, itemId: string) => {
+  return async (code: string, itemId: string) => {
     console.log('Delete coupon code', code, itemId);
 
-    return postgres
-      .exec(sql<CouponCode[]>`
-      UPDATE content.coupon_code
-        SET deleted_at = NOW()
-        WHERE code = ${code}
-        AND item_id = ${itemId}
-        RETURNING *;
-    `)
-      .then(firstRow)
-      .then(rejectOnEmpty);
+    return deleteCoupon(postgres, code, itemId);
   };
+};
+
+const deleteCoupon = async (
+  postgres: Dependencies['postgres'],
+  code: string,
+  itemId: string,
+) => {
+  const deleted = await postgres
+    .exec(sql<CouponCode[]>`
+      DELETE FROM content.coupon_code
+      WHERE code = ${code}
+      AND item_id = ${itemId}
+      AND uses = 0
+      RETURNING *;
+    `)
+    .then(firstRow);
+
+  if (deleted) {
+    return deleted;
+  }
+
+  return postgres
+    .exec(sql<CouponCode[]>`
+      UPDATE content.coupon_code
+      SET deleted_at = NOW()
+      WHERE code = ${code}
+      AND item_id = ${itemId}
+      RETURNING *;
+    `)
+    .then(firstRow)
+    .then(rejectOnEmpty);
 };
